6918f47a4903a6a903a436c2a1027d0306bcee87
Hiroki Sakuma <hirokisakuma@HirokinoMacBook-puro.local>
2018-09-18 02:56:08 +0900

    update

--- svhn_acnn.py
+++ svhn_acnn.py
@@ -4,6 +4,8 @@ from __future__ import print_function
 
 import tensorflow as tf
 import numpy as np
+import matplotlib
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 import cv2
