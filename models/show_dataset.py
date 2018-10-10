c62933e60aeb2d328ac2521218b30957475146d3
Hiroki Sakuma <hirokisakuma@HirokinoMacBook-puro.local>
2018-10-10 18:27:37 +0900

    updte

---
+++ models/show_dataset.py
@@ -0,0 +1,19 @@
+import tensorflow as tf
+import argparse
+import os
+import glob
+import cv2
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--filename", type=str, required=True, help="tfrecord filename")
+args = parser.parse_args()
+
+for record  in tf.python_io.tf_record_iterator(args.filename):
+    example = tf.train.Example()
+    example.ParseFromString(record)
+
+    image = example.features.feature["path"].bytes_list.value[0]
+    image = cv2.imread(image)
+ 
+    cv2.imshow("", image)
+    cv2.waitKey(1000)
