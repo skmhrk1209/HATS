43033e54cf31e430667f68f0860f0fecb6078b22
Hiroki Sakuma <hirokisakuma@HirokinoMacBook-puro.local>
2018-10-10 21:13:44 +0900

    update

--- preprocess.py
+++ preprocess.py
@@ -9,10 +9,9 @@ filenames = glob.glob("/home/sakuma/data/imagenet/*/*/*")
 
 for filename in filenames:
 
-    image = cv2.imread(filename)
-    image = cv2.resize(image, (224, 224))
+    image = tf.keras.preprocessing.image.load_img(filename, target_size=(224, 224))
+    image = tf.keras.preprocessing.image.img_to_array(image)
     image = np.expand_dims(image, axis=0)
     image = tf.keras.applications.resnet50.preprocess_input(image)
-
-    cv2.imwrite(filename.replace("imagenet", "preprocessed_imagenet"), image[0])
+    tf.keras.preprocessing.image.save_img(filename.replace("imagenet", "preprocessed_imagenet"), image[0])
     print("{} processed".format(filename))
