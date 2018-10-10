ab292ad705e76c9faab6e2cedc8563a02115fac4
Hiroki Sakuma <hirokisakuma@HirokinoMacBook-puro.local>
2018-10-10 21:31:00 +0900

    update

--- preprocess.py
+++ preprocess.py
@@ -1,11 +1,16 @@
 import tensorflow as tf
 import numpy as np
+import argparse
 import glob
-import cv2
+import os
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--imagenet_dir", type=str, default="/home/sakuma/data/imagenet", help="model directory")
+args = parser.parse_args()
 
 resnet50 = tf.keras.applications.resnet50.ResNet50(weights="imagenet", include_top=False)
 
-filenames = glob.glob("/home/sakuma/data/imagenet/*/*/*")
+filenames = glob.glob(os.path.join(args.imagenet_dir, "/*/*/*.JPEG"))
 
 for filename in filenames:
 
@@ -13,5 +18,5 @@ for filename in filenames:
     image = tf.keras.preprocessing.image.img_to_array(image)
     image = np.expand_dims(image, axis=0)
     image = tf.keras.applications.resnet50.preprocess_input(image)
-    tf.keras.preprocessing.image.save_img(filename.replace("imagenet", "preprocessed_imagenet"), image[0])
+    np.save(filename.replace("imagenet", "imagenet_preprocessed").replace(".JPEG", ".npy"), image)
     print("{} processed".format(filename))
