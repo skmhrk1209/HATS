577323d36281c0987848ce34418002f48015e71a
Hiroki Sakuma <hirokisakuma@HirokinoMacBook-puro.local>
2018-10-10 18:04:26 +0900

    updte

---
+++ data/dataset.py
@@ -0,0 +1,21 @@
+import tensorflow as tf
+
+class Dataset(object):
+
+    def __init__(self, filenames, num_epochs, batch_size, buffer_size):
+
+        self.dataset = tf.data.TFRecordDataset(filenames)
+        self.dataset = self.dataset.shuffle(buffer_size)
+        self.dataset = self.dataset.repeat(num_epochs)
+        self.dataset = self.dataset.map(self.parse)
+        self.dataset = self.dataset.batch(batch_size)
+        self.dataset = self.dataset.prefetch(1)
+        self.iterator = self.dataset.make_one_shot_iterator()
+
+    def parse(self, example):
+
+        raise NotImplementedError()
+
+    def get_next(self):
+
+        return self.iterator.get_next()
---
+++ data/imagenet.py
@@ -0,0 +1,30 @@
+import tensorflow as tf
+import dataset
+
+class Dataset(dataset.Dataset):
+
+    def parse(self, example):
+
+        features = tf.parse_single_example(
+            serialized=example,
+            features={
+                "path": tf.FixedLenFeature(
+                    shape=[],
+                    dtype=tf.string,
+                    default_value=""
+                ),
+                "label": tf.FixedLenFeature(
+                    shape=[],
+                    dtype=tf.int64,
+                    default_value=0
+                )
+            }
+        )
+
+        image = tf.read_file(features["path"])
+        image = tf.image.decode_jpeg(image, 3)
+        image = tf.image.convert_image_dtype(image, tf.float32)
+
+        label = tf.cast(features["label"], tf.int32)
+
+        return {"image": image}, label
