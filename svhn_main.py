5d7c3fe06372e84010ad856dfb118cfc95987f5d
Hiroki Sakuma <hirokisakuma@HirokinoMacBook-puro.local>
2018-10-13 02:49:49 +0900

    update

--- svhn_main.py
+++ svhn_main.py
@@ -28,11 +28,11 @@ def main(unused_argv):
     imagenet_classifier = tf.estimator.Estimator(
         model_fn=Model(
             convolutional_network=ResidualNetwork(
-                conv_param=AttrDict(filters=32, kernel_size=[3, 3], strides=[2, 2]),
+                conv_param=AttrDict(filters=32, kernel_size=[3, 3], strides=[1, 1]),
                 pool_param=None,
                 residual_params=[
-                    AttrDict(filters=64, strides=[1, 1], blocks=1),
-                    AttrDict(filters=128, strides=[1, 1], blocks=1),
+                    AttrDict(filters=64, strides=[2, 2], blocks=1),
+                    AttrDict(filters=128, strides=[2, 2], blocks=1),
                     AttrDict(filters=256, strides=[1, 1], blocks=1),
                     AttrDict(filters=512, strides=[1, 1], blocks=1),
                 ],
