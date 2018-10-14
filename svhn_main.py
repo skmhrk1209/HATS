b34fc48e86ec9c9c56cd1c15bbcdfeb1d555cb9b
Hiroki Sakuma <hirokisakuma@HirokinoMacBook-puro.local>
2018-10-14 21:03:11 +0900

    update

--- svhn_main.py
+++ svhn_main.py
@@ -133,17 +133,8 @@ def main(unused_argv):
                 
             attention_maps = np.apply_along_axis(np.sum, axis=2, arr=attention_maps)
             attention_maps = scale(attention_maps, attention_maps.min(), attention_maps.max(), 0, 1)
-            attention_maps = np.expand_dims(attention_maps, axis=2)
-            attention_maps = cv2.resize(attention_maps, (128, 128))
 
-            attention_maps = np.pad(
-                array=attention_maps,
-                pad_width=[[0, 0], [0, 0], [0, 2]],
-                mode="constant",
-                constant_values=0
-            )
-
-            cv2.imwrite("image_{}.png".format(i), (features + attention_maps) * 255.)
+            cv2.imwrite("image_{}.png".format(i), attention_maps * 255.)
 
 
 if __name__ == "__main__":
