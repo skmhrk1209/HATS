9238dc9c16bfeabbb13b2c1dbf7e4df551d31aae
Hiroki Sakuma <hirokisakuma@HirokinoMacBook-puro.local>
2018-10-16 11:43:39 +0900

    update

--- svhn_main.py
+++ svhn_main.py
@@ -33,10 +33,10 @@ def main(unused_argv):
                 conv_param=AttrDict(filters=32, kernel_size=[3, 3], strides=[2, 2]),
                 pool_param=None,
                 residual_params=[
-                    AttrDict(filters=32, strides=[1, 1], blocks=1),
-                    AttrDict(filters=64, strides=[1, 1], blocks=1),
-                    AttrDict(filters=128, strides=[1, 1], blocks=1),
-                    AttrDict(filters=256, strides=[1, 1], blocks=1),
+                    AttrDict(filters=32, strides=[1, 1], blocks=2),
+                    AttrDict(filters=64, strides=[1, 1], blocks=2),
+                    AttrDict(filters=128, strides=[1, 1], blocks=2),
+                    AttrDict(filters=256, strides=[1, 1], blocks=2),
                 ],
                 num_classes=None,
                 data_format=args.data_format
