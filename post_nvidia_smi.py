0044d22f6fe43b88988dc0b05ea436a9c3ef59ca
Hiroki Sakuma <hirokisakuma@HirokinoMacBook-puro.local>
2018-10-18 14:18:13 +0900

    update

---
+++ post_nvidia_smi.py
@@ -0,0 +1,23 @@
+import json
+import requests
+import subprocess
+
+url = "https://hooks.slack.com/services/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"
+
+
+def read_lines(command):
+    return subprocess.Popen(
+        command,
+        stdout=subprocess.PIPE,
+        shell=True
+    ).stdout.readlines()
+
+
+def post_slack(url, text):
+
+    return requests.post(url, data=json.dumps({"text": text}))
+
+
+if __name__ == "__main__":
+
+    post_slack(url, "".join([line.decode("utf-8") + "\n" for line in read_lines("nvidia-smi")]))
