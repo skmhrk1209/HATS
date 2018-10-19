e6cab88fa4906cc7ecb526c26e7385e36a7e33ff
Hiroki Sakuma <hirokisakuma@HirokinoMacBook-puro.local>
2018-10-19 12:08:02 +0900

    update

--- svhn_main.py
+++ svhn_main.py
@@ -124,8 +124,7 @@ def main(unused_argv):
             feature = predict_result["features"]
             reduced_attention_map = predict_result["reduced_attention_maps"]
             reduced_attention_map = cv2.resize(reduced_attention_map, feature.shape[:-1])
-            #feature[:, :, -1] += reduced_attention_map[:, :, 0]
-            print(feature)
+            feature[:, :, -1] += reduced_attention_map[:, :, 0]
             cv2.imwrite("outputs/image_{}.png".format(i), feature * 255.)
 
 if __name__ == "__main__":
