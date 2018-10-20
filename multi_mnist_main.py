1864774c409f524e25e0ec0f2c3182ae07fdca58
Hiroki Sakuma <hirokisakuma@HirokinoMacBook-puro.local>
2018-10-21 04:00:58 +0900

    update

--- multi_mnist_main.py
+++ multi_mnist_main.py
@@ -100,7 +100,7 @@ def main(unused_argv):
             data_format=args.data_format,
             hyper_params=AttrDict(
                 cross_entropy_decay=1e-0,
-                attention_map_decay=1e-1,
+                attention_map_decay=1e-2,
                 total_variation_decay=0e-0
             )
         ),
