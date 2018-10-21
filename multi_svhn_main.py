4e6e398ac2f2318f6982ac9548c6086042f95a2c
Hiroki Sakuma <hirokisakuma@HirokinoMacBook-puro.local>
2018-10-21 11:26:16 +0900

    update

--- multi_svhn_main.py
+++ multi_svhn_main.py
@@ -60,8 +60,8 @@ def main(unused_argv):
             data_format=args.data_format,
             hyper_params=AttrDict(
                 cross_entropy_decay=1e-0,
-                attention_map_decay=1e-1,
-                total_variation_decay=0e-0
+                attention_map_decay=1e-2,
+                total_variation_decay=1e-4
             )
         ),
         model_dir=args.model_dir,
