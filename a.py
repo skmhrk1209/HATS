dac96ac9e61b432d182f3e80bc06640fe73d9cba
Hiroki Sakuma <hirokisakuma@HirokinoMacBook-puro.local>
2018-10-25 17:28:41 +0900

    update

---
+++ a.py
@@ -0,0 +1,9 @@
+import tensorflow as tf
+
+cell1 = tf.nn.rnn_cell.LSTMCell(10)
+cell2 = tf.nn.rnn_cell.LSTMCell(10)
+
+x = tf.zeros([10, 10])
+
+y1 = tf.nn.static_rnn(cell1, x, dtype=tf.float32)
+y2 = tf.nn.static_rnn(cell1, x, dtype=tf.float32)
