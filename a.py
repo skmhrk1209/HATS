198a59f50a4df85f8302693b50b37987277e8a2b
Hiroki Sakuma <hirokisakuma@HirokinoMacBook-puro.local>
2018-10-25 17:29:57 +0900

    update

--- a.py
+++ a.py
@@ -7,3 +7,9 @@ x = tf.zeros([10, 10])
 
 y1 = tf.nn.static_rnn(cell1, [x], dtype=tf.float32)
 y2 = tf.nn.static_rnn(cell1, [x], dtype=tf.float32)
+
+with tf.Session() as sess:
+
+    sess.run(tf.global_variables_initializer())
+
+    sess.run([y1, y2])
