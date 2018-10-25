663652bcdd030eda9d231cead29cf57e66306976
Hiroki Sakuma <hirokisakuma@HirokinoMacBook-puro.local>
2018-10-25 17:32:00 +0900

    update

--- a.py
+++ a.py
@@ -5,8 +5,9 @@ cell2 = tf.nn.rnn_cell.LSTMCell(10)
 
 x = tf.zeros([10, 10])
 
-y1 = tf.nn.static_rnn(cell1, [x], dtype=tf.float32)
-y2 = tf.nn.static_rnn(cell2, [x], dtype=tf.float32)
+y1 = tf.nn.static_rnn(cell1, [x], dtype=tf.float32, scope="a")
+y2 = tf.nn.static_rnn(cell2, [x], dtype=tf.float32, scope="b")
+y3 = tf.nn.static_rnn(cell2, [x], dtype=tf.float32, scope="b")
 
 with tf.Session() as sess:
 
