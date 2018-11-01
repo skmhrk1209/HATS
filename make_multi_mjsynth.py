5be8758aee12ef48f3a0ea1be124d875b13c90d8
Hiroki Sakuma <hirokisakuma@HirokinoMacBook-puro.local>
2018-11-01 22:51:03 +0900

    update

--- make_multi_mjsynth.py
+++ make_multi_mjsynth.py
@@ -3,66 +3,78 @@ import cv2
 import glob
 import os
 import shutil
-
-filenames = glob.glob("/home/sakuma/data/synth/*/*/*.jpg")
-print(len(filenames))
-filenames = [filename for filename in filenames if (lambda image: (image is not None) and (image.shape[1] <= 128))(cv2.imread(filename))]
-print(len(filenames))
-
-for i, filename in enumerate(filenames[:int(len(filenames) * 0.9)]):
-
-    shutil.move(filename, "/home/sakuma/data/mjsynth/train/{}_{}.jpg".format(i, filename.split("_")[1]))
-
-for i, filename in enumerate(filenames[int(len(filenames) * 0.9):]):
-
-    shutil.move(filename, "/home/sakuma/data/mjsynth/test/{}_{}.jpg".format(i, filename.split("_")[1]))
+import shapely
 
 filenames = glob.glob("/home/sakuma/data/mjsynth/train/*.jpg")
-print(len(filenames))
 
-for i in range(900000):
+for i in range(90000):
 
-    random_filenames = np.random.choice(filenames, np.random.randint(1, 5))
-    labels = "_".join([os.path.splitext(random_filename)[0].split("_")[1] for random_filename in random_filenames])
-    random_filenames = np.pad(random_filenames, [0, 4 - len(random_filenames)], "constant", constant_values="")
-    np.random.shuffle(random_filenames)
+    num_strings ＝ np.random.randint(1, 5)
+    random_filenames = np.random.choice(filenames, num_strings)
     image = np.zeros([256, 256, 3], dtype=np.uint8)
 
-    for i, random_filename in enumerate(random_filenames):
+    boxes = []
 
-        if not random_filename:
-            continue
+    for random_filename in random_filenames:
 
         random_image = cv2.imread(random_filename)
 
-        x = np.random.randint(0, 256 - random_image.shape[1])
-        y = np.random.randint(64 * i, 64 * (i + 1) - random_image.shape[0])
+        while True:
 
-        image[y: y + random_image.shape[0], x: x + random_image.shape[1], :] = random_image
+            h = random_image.shape[0]
+            w = random_image.shape[1]
+            x = np.random.randint(0, 256 - w)
+            y = np.random.randint(0, 256 - h)
+            proposal = shapely.geometry.box(x, y, x + w, y + h)
+
+            for box in boxes:
+
+                if proposal.intersects(rect):
+                    break
+
+            else:
+
+                image[y:y+h, x:x+w, :] += random_image
+                boxes.append(proposal)
+
+    random_filenames = [pair[0] for pair in sorted(zip(random_filenames, boxes), key=lambda pair: pair[1])]
+    labels = "_".join([os.path.splitext(random_filename)[0].split("_")[1] for random_filename in random_filenames])
 
     cv2.imwrite("/home/sakuma/data/multi_mjsynth/train/{}_{}.jpg".format(i, labels), image)
 
 filenames = glob.glob("/home/sakuma/data/mjsynth/test/*.jpg")
-print(len(filenames))
 
-for i in range(100000):
+for i in range(10000):
 
-    random_filenames = np.random.choice(filenames, np.random.randint(1, 5))
-    random_filenames = np.pad(random_filenames, [0, 4 - len(random_filenames)], "constant", constant_values="")
-    np.random.shuffle(random_filenames)
-    labels = "_".join([os.path.splitext(random_filename)[0].split("_")[1] for random_filename in random_filenames if random_filename])
+    num_strings ＝ np.random.randint(1, 5)
+    random_filenames = np.random.choice(filenames, num_strings)
     image = np.zeros([256, 256, 3], dtype=np.uint8)
 
-    for i, random_filename in enumerate(random_filenames):
+    boxes = []
 
-        if not random_filename:
-            continue
+    for random_filename in random_filenames:
 
         random_image = cv2.imread(random_filename)
 
-        x = np.random.randint(0, 256 - random_image.shape[1])
-        y = np.random.randint(64 * i, 64 * (i + 1) - random_image.shape[0])
+        while True:
 
-        image[y: y + random_image.shape[0], x: x + random_image.shape[1], :] = random_image
+            h = random_image.shape[0]
+            w = random_image.shape[1]
+            x = np.random.randint(0, 256 - w)
+            y = np.random.randint(0, 256 - h)
+            proposal = shapely.geometry.box(x, y, x + w, y + h)
+
+            for box in boxes:
+
+                if proposal.intersects(rect):
+                    break
+
+            else:
+
+                image[y:y+h, x:x+w, :] += random_image
+                boxes.append(proposal)
+
+    random_filenames = [pair[0] for pair in sorted(zip(random_filenames, boxes), key=lambda pair: pair[1])]
+    labels = "_".join([os.path.splitext(random_filename)[0].split("_")[1] for random_filename in random_filenames])
 
     cv2.imwrite("/home/sakuma/data/multi_mjsynth/test/{}_{}.jpg".format(i, labels), image)
