b337d1c087bfd66705b52cd2cf422f62a2f773c4
Hiroki Sakuma <hirokisakuma@HirokinoMacBook-puro.local>
2018-11-05 08:59:22 +0900

    update

--- make_multi_mjsynth.py
+++ make_multi_mjsynth.py
@@ -5,8 +5,8 @@ import os
 import shutil
 from operator import itemgetter
 from operator import attrgetter
-from shapely.geometry import box
 from numba import jit
+from shapely.geometry import box
 
 
 @jit
@@ -36,16 +36,17 @@ def make_multi_mjsynth(mjsynth_dir, image_size, num_data, sequence_length):
 
         num_strings = np.random.random_integers(1, sequence_length)
         random_filenames = np.random.choice(filenames, num_strings)
-        image = np.zeros(image_size + [3], dtype=np.uint8)
+        image = np.zeros(image_size + (3,), dtype=np.uint8)
 
         random_rects = []
+        remove_indices = []
 
-        for j, random_filename in enumerate(random_filenames[:]):
+        for j, random_filename in enumerate(random_filenames):
 
             random_image = cv2.imread(random_filename)
 
             if random_image.shape[0] > image_size[0] or random_image.shape[1] > image_size[1]:
-                random_filenames = np.delete(random_filenames, j)
+                remove_indices.append(j)
                 continue
 
             while True:
@@ -54,7 +55,7 @@ def make_multi_mjsynth(mjsynth_dir, image_size, num_data, sequence_length):
                 w = random_image.shape[1]
                 y = np.random.random_integers(0, image_size[0] - h)
                 x = np.random.random_integers(0, image_size[1] - w)
-                proposal = (x, y, x + w, y + h)
+                proposal = (y, x, y + h, x + w)
 
                 for random_rect in random_rects:
 
@@ -67,6 +68,7 @@ def make_multi_mjsynth(mjsynth_dir, image_size, num_data, sequence_length):
                     random_rects.append(proposal)
                     break
 
+        random_filenames = np.delete(random_filenames, remove_indices)
         random_filenames = [random_filename for random_rect, random_filename in sorted(zip(random_rects, random_filenames))]
         labels = "_".join([os.path.splitext(os.path.basename(random_filename))[0].split("_")[1] for random_filename in random_filenames])
         print(i, labels)
@@ -77,5 +79,5 @@ def make_multi_mjsynth(mjsynth_dir, image_size, num_data, sequence_length):
 if __name__ == "__main__":
 
     # prepare_mjsynth("../../Downloads/mjsynth/*/*", 10, 0.9)
-    make_multi_mjsynth("../../Downloads/mjsynth/train", [256, 256], 90000, 4)
-    make_multi_mjsynth("../../Downloads/mjsynth/test", [256, 256], 10000, 4)
+    make_multi_mjsynth("../../Downloads/mjsynth/train", (256, 256), 90000, 4)
+    make_multi_mjsynth("../../Downloads/mjsynth/test", (256, 256), 10000, 4)
