a6dab06c3cb29ea61de4fdef98d1f3fdbc20aafa
Hiroki Sakuma <hirokisakuma@HirokinoMacBook-puro.local>
2018-11-06 21:38:40 +0900

    update

--- make_multi_mjsynth.py
+++ make_multi_mjsynth.py
@@ -12,12 +12,9 @@ from shapely.geometry import box
 
 def make_multi_thread(func, num_threads):
 
-    def func_mt(arg, *args):
+    def func_mt(*args):
 
-        chnk_size = len(arg) / num_threads
-        chunk_args = [arg[chnk_size * i:chnk_size * (i + 1)] for i in range(num_threads)]
-
-        threads = [threading.Thread(target=func, args=((chunk_arg,) + args)) for chunk_arg in chunk_args]
+        threads = [threading.Thread(target=func, args=args) for _ in range(num_threads)]
 
         for thread in threads:
             thread.start()
