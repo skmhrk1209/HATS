ab5bf7255d4f1b8b8831b07403d18d4bd631c1b1
Hiroki Sakuma <hirokisakuma@HirokinoMacBook-puro.local>
2018-11-08 00:04:33 +0900

    update

--- make_multi_mjsynth.py
+++ make_multi_mjsynth.py
@@ -1,7 +1,8 @@
 import numpy as np
 import cv2
-import glob
 import os
+import glob
+import shutil
 import random
 import threading
 import itertools
@@ -10,11 +11,35 @@ from tqdm import trange
 from shapely.geometry import box
 
 
-def make_multi_thread(func, num_threads):
+def make_multi_thread(func, num_threads, split):
 
-    def func_mt(*args):
+    def func_mt(arg, *args, **kwargs):
 
-        threads = [threading.Thread(target=func, args=args + (i,)) for i in range(num_threads)]
+        def merge(*ds):
+            md = {}
+            for d in ds:
+                md.update(d)
+            return md
+
+        if split:
+
+            chunk_len = len(arg) / num_threads
+            chunk_args = [arg[chunk_len * i: chunk_len * (i + 1)] for i in range(num_threads)[:-1]]
+            chunk_args += [arg[chunk_len * i:] for i in range(num_threads)[-1:]]
+
+            threads = [threading.Thread(
+                target=func,
+                args=(chunk_arg,) + args,
+                kwargs=merge(kwargs, {"thread_id": i})
+            ) for i, chunk_arg in enumerate(chunk_args)]
+
+        else:
+
+            threads = [threading.Thread(
+                target=func,
+                args=(arg,) + args,
+                kwargs=merge(kwargs, {"thread_id": i})
+            ) for i in range(num_threads)]
 
         for thread in threads:
             thread.start()
@@ -26,9 +51,28 @@ def make_multi_thread(func, num_threads):
 
 
 @jit(nopython=False, nogil=True)
-def make_multi_mjsynth(filenames, num_data, image_size, sequence_length, num_retries, thread_id):
+def prepare_mjsynth(filenames, directory, image_size, string_length, thread_id):
 
-    for i in trange(num_data * thread_id, num_data * (thread_id + 1)):
+    os.mkdir(os.path.join(directory, "{}".format(thread_id)))
+
+    filenames = [
+        filename for filename in filenames if
+        (lambda string: len(string) <= string_length)(os.path.splitext(os.path.basename(filename))[0].split("_")[1]) and
+        (lambda image: image and image.shape[0] <= image_size[0] and image.shape[1] <= image_size[1])(cv2.imread(filename))
+    ]
+
+    for i, filename in enumerate(filenames):
+
+        string = os.path.splitext(os.path.basename(filename))[0].split("_")[1]
+        shutil.copy(filename, os.path.join(directory, "{}".format(thread_id), "{}_{}.jpg".format(i, string)))
+
+
+@jit(nopython=False, nogil=True)
+def make_multi_mjsynth(filenames, directory, num_data, image_size, sequence_length, num_retries, thread_id):
+
+    os.mkdir(os.path.join(directory, "{}".format(thread_id)))
+
+    for i in trange(num_data):
 
         multi_image = np.zeros(image_size + [3], dtype=np.uint8)
         num_strings = random.randint(1, sequence_length)
@@ -38,13 +82,8 @@ def make_multi_mjsynth(filenames, num_data, image_size, sequence_length, num_ret
 
         for _ in range(num_strings):
 
-            while True:
-
-                random_filename = random.sample(filenames, 1)
-                image = cv2.imread(random_filename)
-
-                if image and image.shape[0] <= image_size[0] and image.shape[1] <= image_size[1]:
-                    break
+            random_filename = random.sample(filenames, 1)
+            image = cv2.imread(random_filename)
 
             for _ in range(num_retries):
 
@@ -67,12 +106,50 @@ def make_multi_mjsynth(filenames, num_data, image_size, sequence_length, num_ret
                     break
 
         random_filenames = [random_filename for random_rect, random_filename in sorted(zip(random_rects, random_filenames))]
-        labels = "_".join([os.path.splitext(os.path.basename(random_filename))[0].split("_")[1] for random_filename in random_filenames])
+        strings = "_".join([os.path.splitext(os.path.basename(random_filename))[0].split("_")[1] for random_filename in random_filenames])
 
-        cv2.imwrite(os.path.join(os.path.dirname(filenames[0]).replace("mjsynth", "multi_mjsynth"), "{}_{}.jpg".format(i, labels)), multi_image)
+        cv2.imwrite(os.path.join(directory, "{}".format(thread_id), "{}_{}.jpg".format(i, strings)), multi_image)
 
 
 if __name__ == "__main__":
 
-    make_multi_thread(make_multi_mjsynth, 32)(glob.glob("/home/sakuma/data/mjsynth/train/*"), 3000, [256, 256], 4, 100)
-    make_multi_thread(make_multi_mjsynth, 32)(glob.glob("/home/sakuma/data/mjsynth/test/*"), 300, [256, 256], 4, 100)
+    filenames = glob.glob("/home/sakuma/data/synth/*/*/*.jpg")
+    random.shuffle(filenames)
+
+    make_multi_thread(prepare_mjsynth, num_threads=32, split=True)(
+        filenames[:int(len(filenames) * 0.9)],
+        directory="/home/sakuma/data/mjsynth/train",
+        image_size=[128, 128],
+        string_length=10
+    )
+
+    make_multi_thread(prepare_mjsynth, num_threads=32, split=True)(
+        filenames[int(len(filenames) * 0.9):],
+        directory="/home/sakuma/data/mjsynth/test",
+        image_size=[128, 128],
+        string_length=10
+    )
+
+    directory = "/home/sakuma/data/mjsynth/train"
+    filenames = glob.glob(os.path.join(directory, "*.jpg"))
+
+    make_multi_thread(make_multi_mjsynth, num_threads=32, split=False)(
+        filenames,
+        directory=directory.replace("mjsynth, multi_mjsynth"),
+        num_data=3000,
+        image_size=[128, 128],
+        sequence_length=4,
+        num_retries=100
+    )
+
+    directory = "/home/sakuma/data/mjsynth/test"
+    filenames = glob.glob(os.path.join(directory, "*.jpg"))
+
+    make_multi_thread(make_multi_mjsynth, num_threads=32, split=False)(
+        filenames,
+        directory=os.path.dirname(filenames).replace("mjsynth, multi_mjsynth"),
+        num_data=300,
+        image_size=[128, 128],
+        sequence_length=4,
+        num_retries=100
+    )
