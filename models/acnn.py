be97a45f2d6f985e3b2885073d0c22440dc9b4f6
Hiroki Sakuma <hirokisakuma@HirokinoMacBook-puro.local>
2018-11-09 04:08:15 +0900

    update

--- models/acnn.py
+++ models/acnn.py
@@ -18,9 +18,10 @@ class Model(object):
 
         images = features["image"]
 
-        print(images.shape)
         images.set_shape([self.hyper_params.batch_size] + images.get_shape().as_list()[1:])
         labels.set_shape([self.hyper_params.batch_size] + labels.get_shape().as_list()[1:])
+
+        print(images.shape)
 
         feature_maps = self.convolutional_network(
             inputs=images,
