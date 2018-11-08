ddb1131fcb4d15a408ee34c7e6edfaf64706bf35
Hiroki Sakuma <hirokisakuma@HirokinoMacBook-puro.local>
2018-11-08 22:32:03 +0900

    update

--- models/acnn.py
+++ models/acnn.py
@@ -18,45 +18,31 @@ class Model(object):
 
         images = features["image"]
 
-        feature_maps = self.convolutional_network(
-            inputs=images,
-            training=mode == tf.estimator.ModeKeys.TRAIN
-        )
-
         attention_maps_sequence = self.attention_network(
-            inputs=feature_maps,
-            training=mode == tf.estimator.ModeKeys.TRAIN
+            inputs=images,
+            training=mode == tf.estimator.ModeKeys.TRAIN,
+            name="attention_network"
         )
 
-        merged_attention_maps_sequence = [
-            tf.reduce_sum(
-                input_tensor=attention_maps,
-                axis=1 if self.data_format == "channels_first" else 3,
-                keep_dims=True
-            ) for attention_maps in attention_maps_sequence
+        images_sequence = [
+            images * attention_maps
+            for attention_maps in attention_maps_sequence
         ]
 
-        def flatten_images(inputs, data_format):
-
-            input_shape = inputs.get_shape().as_list()
-            output_shape = ([-1, input_shape[1], np.prod(input_shape[2:4])] if self.data_format == "channels_first" else
-                            [-1, np.prod(input_shape[1:3]), input_shape[3]])
-
-            return tf.reshape(inputs, output_shape)
-
-        feature_vectors_sequence = [
-            tf.matmul(
-                a=flatten_images(feature_maps, self.data_format),
-                b=flatten_images(attention_maps, self.data_format),
-                transpose_a=False if self.data_format == "channels_first" else True,
-                transpose_b=True if self.data_format == "channels_first" else False
-            ) for attention_maps in attention_maps_sequence
+        feature_maps_sequence = [
+            self.convolutional_network(
+                inputs=images,
+                training=mode == tf.estimator.ModeKeys.TRAIN,
+                name="convolutional_network",
+                reuse=tf.AUTO_REUSE
+            ) for images in images_sequence
         ]
 
         feature_vectors_sequence = [
-            tf.layers.flatten(
-                inputs=feature_vectors
-            ) for feature_vectors in feature_vectors_sequence
+            tf.reduce_mean(
+                input_tensor=feature_maps,
+                axis=[2, 3] if self.data_format == "channels_first" else [1, 2]
+            ) for feature_maps in feature_maps_sequence
         ]
 
         multi_logits_sequence = [[
@@ -80,8 +66,8 @@ class Model(object):
                 mode=mode,
                 predictions={
                     *{"images": images},
-                    *{"merged_attention_maps_{}".format(i): merged_attention_maps
-                      for i, merged_attention_maps in enumerate(merged_attention_maps_sequence)}
+                    *{"attention_maps_{}".format(i): attention_maps
+                      for i, attention_maps in enumerate(attention_maps_sequence)}
                 }
             )
 
@@ -107,10 +93,10 @@ class Model(object):
             for attention_maps in attention_maps_sequence
         ])
 
-        loss =
-        cross_entropy_loss * self.hyper_params.cross_entropy_decay +
-        attention_map_loss * self.hyper_params.attention_map_decay +
-        total_variation_loss * self.hyper_params.total_variation_decay
+        loss = \
+            cross_entropy_loss * self.hyper_params.cross_entropy_decay + \
+            attention_map_loss * self.hyper_params.attention_map_decay + \
+            total_variation_loss * self.hyper_params.total_variation_decay
 
         multi_classes_sequence = [[
             tf.argmax(logits, axis=-1)
@@ -122,12 +108,13 @@ class Model(object):
             predictions=multi_classes_sequence
         )
 
-        tf.identity(accuracy[0], name="accuracy_value")
+        non_streaming_accuracy = tf.reduce_mean(tf.cast(multi_classes_sequence == multi_labels_sequence, tf.float32))
+        tf.identity(non_streaming_accuracy, name="non_streaming_accuracy")
 
         # ==========================================================================================
-        tf.summary.image("images", images, max_outputs=2)
-        [tf.summary.image("merged_attention_maps_sequence_{}".format(i), merged_attention_maps, max_outputs=2)
-         for i, merged_attention_maps in enumerate(merged_attention_maps_sequence)]
+        tf.summary.image("images", images, max_outputs=4)
+        [tf.summary.image("attention_maps_{}".format(i), attention_maps, max_outputs=4)
+         for i, attention_maps in enumerate(attention_maps_sequence)]
         tf.summary.scalar("cross_entropy_loss", cross_entropy_loss)
         tf.summary.scalar("attention_map_loss", attention_map_loss)
         tf.summary.scalar("total_variation_loss", total_variation_loss)
