8e5c7ebce578b16f6bcebc28ed9930f52c0eb2f6
Hiroki Sakuma <hirokisakuma@HirokinoMacBook-puro.local>
2018-11-09 04:42:05 +0900

    update

--- models/acnn.py
+++ models/acnn.py
@@ -100,7 +100,7 @@ class Model(object):
         ]
 
         for labels in labels_sequence:
-            tf.Print(labels)
+            tf.Print(labels, labels)
 
         sequence_length_sequence = [[
             tf.shape(tf.where(tf.not_equal(dense_label, tf.constant(self.num_classes - 1))))[0]
