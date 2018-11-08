dad6b9fca27ced541f594ff0c55fee3a8990488f
Hiroki Sakuma <hirokisakuma@HirokinoMacBook-puro.local>
2018-11-08 22:33:03 +0900

    update

--- multi_mjsynth_main.py
+++ multi_mjsynth_main.py
@@ -63,8 +63,8 @@ def main(unused_argv):
             data_format=args.data_format,
             hyper_params=AttrDict(
                 cross_entropy_decay=1e-0,
-                attention_map_decay=1e-3,
-                total_variation_decay=1e-6
+                attention_map_decay=1e-6,
+                total_variation_decay=1e-12
             )
         ),
         model_dir=args.model_dir,
