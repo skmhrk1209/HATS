6ec5434dca616b77f7e5f754cfa974545879f942
Hiroki Sakuma <hirokisakuma@HirokinoMacBook-puro.local>
2018-11-09 04:41:14 +0900

    update

--- models/acnn.py
+++ models/acnn.py
@@ -99,6 +99,9 @@ class Model(object):
             for dense_labels in tf.unstack(labels, axis=1)
         ]
 
+        for labels in labels_sequence:
+            tf.print(labels)
+
         sequence_length_sequence = [[
             tf.shape(tf.where(tf.not_equal(dense_label, tf.constant(self.num_classes - 1))))[0]
             for dense_label in tf.unstack(dense_labels, axis=0)
