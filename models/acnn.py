29fba0156aaba647b9fa531ebe44566f44332b94
Hiroki Sakuma <hirokisakuma@HirokinoMacBook-puro.local>
2018-11-09 04:28:38 +0900

    update

--- models/acnn.py
+++ models/acnn.py
@@ -118,10 +118,10 @@ class Model(object):
 
         error_rate = tf.reduce_mean([
             tf.edit_distance(
-                hypothesis=tf.nn.ctc_greedy_decoder(
+                hypothesis=tf.cast(tf.nn.ctc_greedy_decoder(
                     inputs=logits,
                     sequence_length=sequence_length
-                )[0][0],
+                )[0][0], tf.int32),
                 truth=labels,
                 normalize=True
             ) for labels, logits, sequence_length in zip(labels_sequence, logits_sequence, sequence_length_sequence)
