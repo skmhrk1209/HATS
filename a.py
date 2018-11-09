b0422f3106c4054a1ec178ae43a148f8deb4f33e
Hiroki Sakuma <hirokisakuma@HirokinoMacBook-puro.local>
2018-11-10 04:48:47 +0900

    update

--- a.py
+++
@@ -1,6 +0,0 @@
-def enumerate_map_innermost(function, sequence, indices=[]):
-
-    return ([enumerate_map_innermost(function, element, indices + [index]) for index, element in enumerate(sequence)]
-            if isinstance(sequence, list) else function(indices, sequence))
-
-print(enumerate_map_innermost(lambda indices, x: (indices, x), [[1, 2], [4, [3, 1]]]))
