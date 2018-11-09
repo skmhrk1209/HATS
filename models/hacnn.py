11dfa549362c857460b92866f29a64d523ce9304
Hiroki Sakuma <hirokisakuma@HirokinoMacBook-puro.local>
2018-11-09 23:02:45 +0900

    update

--- models/hacnn.py
+++ models/hacnn.py
@@ -24,18 +24,18 @@ class Model(object):
             training=mode == tf.estimator.ModeKeys.TRAIN
         )
 
-        attention_maps_sequence = self.attention_network(
+        attention_maps_sequence_sequence = self.attention_network(
             inputs=feature_maps,
             training=mode == tf.estimator.ModeKeys.TRAIN
         )
 
-        merged_attention_maps_sequence = [
+        merged_attention_maps_sequence_sequence = [[
             tf.reduce_sum(
                 input_tensor=attention_maps,
                 axis=1 if self.data_format == "channels_first" else 3,
                 keep_dims=True
             ) for attention_maps in attention_maps_sequence
-        ]
+        ] for attention_maps_sequence in attention_maps_sequence_sequence]
 
         def flatten_images(inputs, data_format):
 
@@ -45,96 +45,80 @@ class Model(object):
 
             return tf.reshape(inputs, output_shape)
 
-        feature_vectors_sequence = [
+        feature_vectors_sequence_sequence = [[
             tf.layers.flatten(tf.matmul(
                 a=flatten_images(feature_maps, self.data_format),
                 b=flatten_images(attention_maps, self.data_format),
                 transpose_a=False if self.data_format == "channels_first" else True,
                 transpose_b=True if self.data_format == "channels_first" else False
             )) for attention_maps in attention_maps_sequence
-        ]
+        ] for attention_maps_sequence in attention_maps_sequence_sequence]
 
-        lstm_cell_forward = tf.nn.rnn_cell.LSTMCell(
-            num_units=self.lstm_units,
-            use_peepholes=True
-        )
-
-        lstm_cell_backward = tf.nn.rnn_cell.LSTMCell(
-            num_units=self.lstm_units,
-            use_peepholes=True
-        )
-
-        multi_feature_vectors_sequence = [
-            tf.nn.static_bidirectional_rnn(
-                cell_fw=lstm_cell_forward,
-                cell_bw=lstm_cell_backward,
-                inputs=[feature_vectors] * self.string_length,
-                dtype=tf.float32
-            )[0] for feature_vectors in feature_vectors_sequence
-        ]
-
-        multi_logits_sequence = [[
+        logits_sequence_sequence = [[
             tf.layers.dense(
                 inputs=feature_vectors,
-                units=self.num_classes
-            ) for feature_vectors in multi_feature_vectors
-        ] for multi_feature_vectors in multi_feature_vectors_sequence]
+                units=self.num_classes,
+                name="logits",
+                reuse=tf.AUTO_REUSE
+            ) for feature_vectors in feature_vectors_sequence
+        ] for feature_vectors_sequence in feature_vectors_sequence_sequence]
 
-        multi_classes_sequence = [[
+        predictions_sequence_sequence = [[
             tf.argmax(logits, axis=-1, output_type=tf.int32)
-            for logits in multi_logits
-        ] for multi_logits in multi_logits_sequence]
+            for logits in logits_sequence
+        ] for logits_sequence in logits_sequence_sequence]
 
-        multi_labels_sequence = [
-            tf.unstack(multi_labels, axis=1)
-            for multi_labels in tf.unstack(labels, axis=1)
+        labels_sequence_sequence = [
+            tf.unstack(labels, axis=1)
+            for labels in tf.unstack(labels, axis=1)
         ]
 
         if mode == tf.estimator.ModeKeys.PREDICT:
 
             return tf.estimator.EstimatorSpec(
                 mode=mode,
-                predictions=dict(
-                    {"images": images},
-                    *{"merged_attention_maps_{}".format(i): merged_attention_maps
-                      for i, merged_attention_maps in enumerate(merged_attention_maps_sequence)}
-                )
+                predictions={}
             )
 
         cross_entropy_loss = tf.reduce_mean([[
             tf.losses.sparse_softmax_cross_entropy(
                 labels=labels,
                 logits=logits
-            ) for labels, logits in zip(multi_labels, multi_logits)
-        ] for multi_labels, multi_logits in zip(multi_labels_sequence, multi_logits_sequence)])
+            ) for labels, logits in zip(labels_sequence, logits_sequence)
+        ] for labels_sequence, logits_sequence in zip(labels_sequence_sequence, logits_sequence_sequence)])
 
-        attention_map_loss = tf.reduce_mean([
+        attention_map_loss = tf.reduce_mean([[
             tf.reduce_mean(tf.reduce_sum(tf.abs(attention_maps), axis=[1, 2, 3]))
             for attention_maps in attention_maps_sequence
-        ])
+        ] for attention_maps_sequence in attention_maps_sequence_sequence])
 
-        total_variation_loss = tf.reduce_mean([
+        total_variation_loss = tf.reduce_mean([[
             tf.reduce_mean(tf.image.total_variation(attention_maps))
             for attention_maps in attention_maps_sequence
-        ])
+        ] for attention_maps_sequence in attention_maps_sequence_sequence])
 
         loss = \
             cross_entropy_loss * self.hyper_params.cross_entropy_decay + \
             attention_map_loss * self.hyper_params.attention_map_decay + \
             total_variation_loss * self.hyper_params.total_variation_decay
 
-        classes = tf.stack([
-            tf.stack(multi_classes, axis=1)
-            for multi_classes in multi_classes_sequence
+        labels = tf.stack([
+            tf.stack(labels_sequence, axis=1)
+            for labels_sequence in labels_sequence_sequence
+        ], axis=1)
+
+        predictions = tf.stack([
+            tf.stack(predictions_sequence, axis=1)
+            for predictions_sequence in predictions_sequence_sequence
         ], axis=1)
 
         streaming_accuracy = tf.metrics.accuracy(
             labels=labels,
-            predictions=classes
+            predictions=predictions
         )
 
         non_streaming_accuracy = tf.reduce_mean(
-            input_tensor=tf.cast(tf.equal(labels, classes), tf.float32),
+            input_tensor=tf.cast(tf.equal(labels, predictions), tf.float32),
             name="non_streaming_accuracy"
         )
 
