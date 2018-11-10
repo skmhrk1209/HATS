fd74a3697cdc15f9634888f723b524614ada6a4d
Hiroki Sakuma <hirokisakuma@HirokinoMacBook-puro.local>
2018-11-11 03:39:48 +0900

    update

--- multi_mjsynth_main.py
+++ multi_mjsynth_main.py
@@ -43,6 +43,7 @@ def main(unused_argv):
             ),
             attention_network=AttentionNetwork(
                 conv_params=[
+                    # kernel size shuld be large 
                     AttrDict(filters=4, kernel_size=[9, 9], strides=[2, 2]),
                     AttrDict(filters=4, kernel_size=[9, 9], strides=[2, 2]),
                 ],
@@ -91,12 +92,12 @@ def main(unused_argv):
             ),
             attention_network=AttentionNetwork(
                 conv_params=[
-                    AttrDict(filters=4, kernel_size=[9, 9], strides=[2, 2]),
+                    AttrDict(filters=16, kernel_size=[9, 9], strides=[2, 2]),
                     AttrDict(filters=4, kernel_size=[9, 9], strides=[2, 2]),
                 ],
                 deconv_params=[
-                    AttrDict(filters=16, kernel_size=[9, 9], strides=[2, 2]),
-                    AttrDict(filters=16, kernel_size=[9, 9], strides=[2, 2]),
+                    AttrDict(filters=4, kernel_size=[3, 3], strides=[2, 2]),
+                    AttrDict(filters=16, kernel_size=[3, 3], strides=[2, 2]),
                 ],
                 rnn_params=[
                     AttrDict(sequence_length=4, num_units=[256, 256]),
