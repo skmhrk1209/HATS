c52b1fc42ce5cc674af0a7471524262e440e2785
Hiroki Sakuma <hirokisakuma@HirokinoMacBook-puro.local>
2018-11-12 20:32:59 +0900

    update

--- make_mjsynth.py
+++ make_mjsynth.py
@@ -3,6 +3,7 @@ import cv2
 import os
 import glob
 import shutil
+import random
 from tqdm import tqdm
 
 if __name__ == "__main__":
@@ -12,12 +13,14 @@ if __name__ == "__main__":
         if len(os.path.splitext(os.path.basename(filename))[0].split("_")[1]) <= 10
     ]
 
-    for i, filename in enumerate(tqdm(filenames[:int(len(filenames) * 0.9)])):
+    filenames = random.sample(filenames, 96000 + 9600)
+
+    for i, filename in enumerate(tqdm(filenames[:96000])):
 
         shutil.move(filename, os.path.join("/home/sakuma/data/mjsynth/train",
                                            "{}_{}.jpg".format(i, os.path.splitext(os.path.basename(filename))[0].split("_")[1])))
 
-    for i, filename in enumerate(tqdm(filenames[int(len(filenames) * 0.9):])):
+    for i, filename in enumerate(tqdm(filenames[96000:])):
 
         shutil.move(filename, os.path.join("/home/sakuma/data/mjsynth/test",
                                            "{}_{}.jpg".format(i, os.path.splitext(os.path.basename(filename))[0].split("_")[1])))
