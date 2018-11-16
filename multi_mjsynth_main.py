ee8095b56f89d967bb250fee68d74987d7f4986a
Hiroki Sakuma <hirokisakuma@HirokinoMacBook-puro.local>
2018-11-16 18:33:50 +0900

    update

--- multi_mjsynth_main.py
+++ multi_mjsynth_main.py
@@ -138,8 +138,8 @@ def main(unused_argv):
             data_format=args.data_format,
             hyper_params=AttrDict(
                 cross_entropy_decay=lambda global_step: 1e-0,
-                attention_map_decay=lambda global_step: tf.minimum(1e-2, 1e-2), # / num_steps * global_step
-                total_variation_decay=lambda global_step: tf.minimum(1e-6, 1e-6) #  / num_steps * global_step
+                attention_map_decay=lambda global_step: tf.minimum(1e-2, 1e-2 * global_step / num_steps),
+                total_variation_decay=lambda global_step: tf.minimum(1e-6, 1e-6 * global_step / num_steps)
             )
         ),
         model_dir=args.model_dir,
