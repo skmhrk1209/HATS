34af2c37f022fdcc417547aaf1d3efa37eead91b
Hiroki Sakuma <hirokisakuma@HirokinoMacBook-puro.local>
2018-11-19 03:38:26 +0900

    update

--- algorithms/sequence.py
+++ algorithms/sequence.py
@@ -46,13 +46,3 @@ def any_innermost(sequence, **kwargs):
 
     return (any([any_innermost(element, **kwargs) for element in sequence])
             if isinstance(sequence, kwargs.get("classes", list)) else bool(sequence))
-
-
-def nest_depth(sequence, **kwargs):
-    ''' 
-    return maximum of nest depths of innermost elements.
-    innermost element is defined as element which is not instance of "classes" (default: list)
-    '''
-
-    return (max([nest_depth(element, **kwargs) for element in sequence]) + 1
-            if isinstance(sequence, kwargs.get("classes", list)) else 0)
