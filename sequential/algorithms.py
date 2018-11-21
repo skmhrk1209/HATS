4765619d3df2774c5ae5dfdd3ac2509848d33266
Hiroki Sakuma <hirokisakuma@HirokinoMacBook-puro.local>
2018-11-22 06:54:38 +0900

    update

--- sequential/algorithms.py
+++ sequential/algorithms.py
@@ -7,9 +7,9 @@ def map_innermost(function, sequence, predicate):
     return function(sequence) if predicate(sequence) else type(sequence)(map(lambda element: map_innermost(function, element, predicate), sequence))
 
 
-def enumerate_innermost(sequence, indices=(), predicate):
+def enumerate_innermost(sequence, predicate, indices=()):
 
-    return (indices, sequence) if predicate(sequence) else type(sequence)(map(lambda index_element: enumerate_innermost(index_element[1], indices + (index_element[0],), predicate), enumerate(sequence)))
+    return (indices, sequence) if predicate(sequence) else type(sequence)(map(lambda index_element: enumerate_innermost(index_element[1], predicate, indices + (index_element[0],)), enumerate(sequence)))
 
 
 def zip_innermost(sequences, predicate):
