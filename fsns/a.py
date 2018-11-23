5a6a4dd8a060999ebfb46b9b662ecdc9e7a4cb28
Hiroki Sakuma <hirokisakuma@HirokinoMacBook-puro.local>
2018-11-24 05:12:09 +0900

    update

--- fsns/a.py
+++ fsns/a.py
@@ -4,23 +4,23 @@ with tf.Session() as sess:
 
     label = [1, 2, 0, 3, 4, 0, 5, 6, 133, 133]
     indices = tf.cast(tf.squeeze(tf.where(tf.not_equal(label, 133))), tf.int32)
-        label = tf.gather(label, indices)
-        label = tf.concat([[0], label, [0]], axis=0)
-        indices = tf.cast(tf.squeeze(tf.where(tf.equal(label, 0))), tf.int32)
-        indices = tf.stack([indices[:-1], indices[1:]], axis=-1)
-        label = tf.map_fn(
-            fn=lambda indices: tf.pad(
-                tensor=label[indices[0] + 1: indices[1]],
-                paddings=[[0, 4 - (indices[1] - (indices[0] + 1))]],
-                mode="constant",
-                constant_values=133
-            ),
-            elems=indices
-        )
-        label = tf.pad(
-            tensor=label,
-            paddings=[[0, 4 - tf.shape(label)[0]], [0, 0]],
+    label = tf.gather(label, indices)
+    label = tf.concat([[0], label, [0]], axis=0)
+    indices = tf.cast(tf.squeeze(tf.where(tf.equal(label, 0))), tf.int32)
+    indices = tf.stack([indices[:-1], indices[1:]], axis=-1)
+    label = tf.map_fn(
+        fn=lambda indices: tf.pad(
+            tensor=label[indices[0] + 1: indices[1]],
+            paddings=[[0, 4 - (indices[1] - (indices[0] + 1))]],
             mode="constant",
             constant_values=133
-        )
+        ),
+        elems=indices
+    )
+    label = tf.pad(
+        tensor=label,
+        paddings=[[0, 4 - tf.shape(label)[0]], [0, 0]],
+        mode="constant",
+        constant_values=133
+    )
     print(sess.run(label))
