5885288143225494191010a149af6a4c27f2752a
Hiroki Sakuma <hirokisakuma@HirokinoMacBook-puro.local>
2018-11-24 22:21:31 +0900

    update

--- fsns/make_dataset.py
+++ fsns/make_dataset.py
@@ -2,6 +2,7 @@ import tensorflow as tf
 import glob
 import cv2
 import itertools
+from tqdm import trange
 
 
 def parse(example):
@@ -26,7 +27,7 @@ def parse(example):
     return image, label
 
 
-train_filenames = [f for f in glob.glob("/home/sakuma/data/fsns_raw/*") if "test" in f or "validation" in f]
+train_filenames = [f for f in glob.glob("/home/sakuma/data/fsns_raw/*") if "train" in f]
 
 dataset = tf.data.TFRecordDataset(train_filenames)
 dataset = dataset.map(parse)
@@ -34,8 +35,8 @@ next_elem = dataset.make_one_shot_iterator().get_next()
 
 with tf.Session() as sess:
 
-    for i in itertools.count():
+    for i in trange(1100000):
 
         image, label = sess.run(next_elem)
         image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-        cv2.imwrite("/home/sakuma/data/fsns/test/{}_{}.jpg".format(i, label.decode("utf-8")), image)
+        cv2.imwrite("/home/sakuma/data/fsns/train/{} {}.jpg".format(i, label.decode("utf-8")), image)
