eccf90ec5724ea30c34b12ce7d741457dc87d676
Hiroki Sakuma <hirokisakuma@HirokinoMacBook-puro.local>
2018-11-24 17:45:18 +0900

    update

--- fsns/a.py
+++ fsns/a.py
@@ -8,10 +8,9 @@ for record  in tf.python_io.tf_record_iterator("train.tfrecord"):
  
     path = example.features.feature["path"].bytes_list.value[0].decode("utf-8")
     image = cv2.imread(path)
-    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
     label = example.features.feature["label"].int64_list.value
  
     print(path)
     print(label)
     cv2.imshow("", image)
-    cv2.waitKey()
+    if cv2.waitKey() == ord("q"): break
