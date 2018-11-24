971c09dfdd3e7ed43ae116c519468dad42b01962
Hiroki Sakuma <hirokisakuma@HirokinoMacBook-puro.local>
2018-11-25 01:58:25 +0900

    update

--- fsns/make_dataset.py
+++ fsns/make_dataset.py
@@ -27,7 +27,7 @@ def parse(example):
     return image, label
 
 
-train_filenames = [f for f in glob.glob("/home/sakuma/data/fsns_raw/*") if "test" in f or "validation" in f]
+train_filenames = [f for f in glob.glob("/home/sakuma/data/fsns_raw/*") if "train" in f]
 
 dataset = tf.data.TFRecordDataset(train_filenames)
 dataset = dataset.map(parse)
@@ -35,8 +35,11 @@ next_elem = dataset.make_one_shot_iterator().get_next()
 
 with tf.Session() as sess:
 
-    for i in trange(40000):
+    for i in trange(1044868):
 
         image, label = sess.run(next_elem)
         image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-        cv2.imwrite("/home/sakuma/data/fsns/test/{} {}.jpg".format(i, label.decode("utf-8")), image)
+        label = label.decode("utf-8")
+        if "_" in label:
+            continue
+        cv2.imwrite("/home/sakuma/data/fsns/train/{}_{}.jpg".format(i, label), image)
