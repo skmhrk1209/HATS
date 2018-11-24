ddfc8ff6968d7922a837e258d5364cce551bb11d
Hiroki Sakuma <hirokisakuma@HirokinoMacBook-puro.local>
2018-11-24 18:59:14 +0900

    update

--- fsns/make_dataset.py
+++ fsns/make_dataset.py
@@ -3,26 +3,28 @@ import glob
 import cv2
 import itertools
 
+
 def parse(example):
 
-        features = tf.parse_single_example(
-            serialized=example,
-            features={
-                "image/encoded": tf.FixedLenFeature(
-                    shape=[],
-                    dtype=tf.string
-                ),
-                "image/text": tf.FixedLenFeature(
-                    shape=[],
-                    dtype=tf.string
-                )
-            }
-        )
+    features = tf.parse_single_example(
+        serialized=example,
+        features={
+            "image/encoded": tf.FixedLenFeature(
+                shape=[],
+                dtype=tf.string
+            ),
+            "image/text": tf.FixedLenFeature(
+                shape=[],
+                dtype=tf.string
+            )
+        }
+    )
 
-        image = tf.image.decode_png(features["image/encoded"], 3)
-        label = features["image/text"]
+    image = tf.image.decode_png(features["image/encoded"], 3)
+    label = features["image/text"]
 
-        return image, label
+    return image, label
+
 
 train_filenames = [f for f in glob.glob("/home/sakuma/data/fsns_raw/*") if "train" in f]
 
@@ -36,4 +38,4 @@ with tf.Session() as sess:
 
         image, label = sess.run(next_elem)
         image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-        cv2.imwrite("{}_{}.jpg".format(i, label.decode("utf-8")), image)
+        cv2.imwrite("/home/sakuma/data/fsns/train/{}_{}.jpg".format(i, label.decode("utf-8")), image)
