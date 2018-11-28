65b2a4322e5ac3063765e4e57bf4022c826d4b72
Hiroki Sakuma <hirokisakuma@HirokinoMacBook-puro.local>
2018-11-29 04:50:39 +0900

    update

--- msw/main_.py
+++ msw/main_.py
@@ -100,7 +100,7 @@ def main(unused_argv):
             accuracy_type=Model.AccuracyType.EDIT_DISTANCE,
             hyper_params=AttrDict(
                 cross_entropy_decay=1.0,
-                attention_map_decay=0.001
+                attention_map_decay=0.0002
             )
         ),
         model_dir=args.model_dir,
