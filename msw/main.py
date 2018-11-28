b8f731e32041199dc519c1c878cfbbf9315409e0
Hiroki Sakuma <hirokisakuma@HirokinoMacBook-puro.local>
2018-11-28 20:41:54 +0900

    update

--- msw/main.py
+++ msw/main.py
@@ -92,7 +92,7 @@ def main(unused_argv):
             num_classes=63,
             num_tiles=1,
             data_format="channels_last",
-            accuracy_type=Model.AccuracyType.FULL_SEQUENCE,
+            accuracy_type=Model.AccuracyType.EDIT_DISTANCE,
             hyper_params=AttrDict(
                 cross_entropy_decay=1.0,
                 attention_map_decay=0.001
