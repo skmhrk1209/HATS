ccf82ceea58da80054ac2bc0f2dfe05d1a0d9dfc
Hiroki Sakuma <hirokisakuma@HirokinoMacBook-puro.local>
2018-11-28 23:48:55 +0900

    update

--- fsns/main.py
+++ fsns/main.py
@@ -70,7 +70,7 @@ def main(unused_argv):
     classifier = tf.estimator.Estimator(
         model_fn=Model(
             convolutional_network=ResidualNetwork(
-                conv_param=AttrDict(filters=64, kernel_size=[7, 7], strides=[1, 1]),
+                conv_param=AttrDict(filters=64, kernel_size=[7, 7], strides=[2, 2]),
                 pool_param=None,
                 residual_params=[
                     AttrDict(filters=64, strides=[1, 1], blocks=2),
