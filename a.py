c0b546b1bf15cdd6472d83e92c747ad8ea84f5f4
Hiroki Sakuma <hirokisakuma@HirokinoMacBook-puro.local>
2018-12-02 01:18:41 +0900

    update

--- a.py
+++
@@ -1,8 +0,0 @@
-import glob
-import cv2
-from tqdm import tqdm
-
-fs = glob.glob("/home/sakuma/data/synth/*/*")
-
-for f in tqdm(fs):
-    cv2.imread(f)
--- model_3.py
+++ model_3.py
@@ -1,5 +1,6 @@
 import tensorflow as tf
 import numpy as np
+import os
 import metrics
 from algorithms import *
 
@@ -28,44 +29,95 @@ class Model(object):
             training=mode == tf.estimator.ModeKeys.TRAIN
         )
 
-        attention_maps = self.attention_network(
-            inputs=feature_maps,
-            training=mode == tf.estimator.ModeKeys.TRAIN
+        def flatten_images(inputs, data_format):
+
+            input_shape = inputs.get_shape().as_list()
+            output_shape = ([-1, input_shape[1], np.prod(input_shape[2:4])] if self.data_format == "channels_first" else
+                            [-1, np.prod(input_shape[1:3]), input_shape[3]])
+
+            return tf.reshape(inputs, output_shape)
+
+        feature_vectors = flatten_images(feature_maps)
+
+        lstm_cell = tf.nn.rnn_cell.LSTMCell(
+            num_units=num_units,
+            use_peepholes=True
         )
 
-        merged_attention_maps = map_innermost_element(
-            function=lambda attention_maps: tf.reduce_sum(
-                input_tensor=attention_maps,
-                axis=1 if self.data_format == "channels_first" else 3,
-                keep_dims=True
+        attention_mechanism = tf.contrib.seq2seq.BahdanauAttention(
+            num_units=num_units,
+            memory=feature_vectors
+        )
+
+        ''' Perform a step of attention-wrapped RNN.
+        - Step 1: Mix the `inputs` and previous step's `attention` output via `cell_input_fn`.
+        - Step 2: Call the wrapped `cell` with this input and its previous state.
+        - Step 3: Score the cell's output with `attention_mechanism`.
+        - Step 4: Calculate the alignments by passing the score through the `normalizer`.
+        - Step 5: Calculate the context vector as the inner product 
+                  between the alignments and the attention_mechanism's values (memory).
+        - Step 6: Calculate the attention output by concatenating the cell output and context 
+                  through the attention layer (a linear layer with `attention_layer_size` outputs).
+        '''
+        attention_lstm_cell = tf.contrib.seq2seq.AttentionWrapper(
+            cell=lstm_cell,
+            attention_mechanism=attention_mechanism,
+            cell_input_fn=lambda inputs, attention: f.layers.dense(
+                inputs=tf.concat([inputs, attention[:, num_units:]], axis=-1),
+                units=num_units
             ),
-            sequence=attention_maps
+            output_attention=True,
+            attention_layer=lambda inputs: inputs
         )
 
-        def global_average_pooling2d(inputs, data_format):
+        sequence_length = tf.map_fn(
+            fn=lambda is_not_blank: tf.count_nonzero(is_not_blank),
+            elems=tf.not_equal(labels, self.num_classes - 1)
+        )
 
-            return tf.reduce_mean(
-                input_tensor=inputs,
-                axis=[2, 3] if data_format == "channels_first" else [1, 2]
+        batch_size = tf.shape(sequence_length)[0]
+        start_tokens = tf.constant(-1, shape=[batch_size])
+
+        if mode == tf.estimator.ModeKeys.TRAIN:
+
+            helper = tf.contrib.seq2seq.TrainingHelper(
+                inputs=tf.one_hot(
+                    indices=tf.concat([tf.expand_dims(start_tokens, axis=1), labels], axis=1),
+                    depth=self.num_classes
+                ),
+                sequence_length=sequence_length,
+                time_major=False
             )
 
-        feature_vectors = map_innermost_element(
-            function=lambda attention_maps: global_average_pooling2d(
-                inputs=feature_maps * attention_maps,
-                data_format=self.data_format
-            ),
-            sequence=attention_maps
+        else:
+
+            helper = tf.contrib.seq2seq.GreedyEmbeddingHelper(
+                embedding=lambda labels: tf.one_hot(
+                    indices=labels,
+                    depth=self.num_classes
+                ),
+                start_tokens=start_tokens,
+                end_token=self.num_classes - 1
+            )
+
+        decoder = tf.contrib.seq2seq.BasicDecoder(
+            cell=attention_lstm_cell,
+            helper=helper,
+            initial_state=attention_lstm_cell.zero_state(batch_size, tf.float32),
+            output_layer=lambda inputs: tf.layers.dense(
+                inputs=inputs,
+                units=self.num_classes
+            )
         )
 
-        logits = map_innermost_element(
-            function=lambda feature_vectors: tf.layers.dense(
-                inputs=feature_vectors,
-                units=self.num_classes,
-                name="logits",
-                reuse=tf.AUTO_REUSE
-            ),
-            sequence=feature_vectors
-        )
+        logits = tf.contrib.seq2seq.dynamic_decode(
+            decoder=decoder,
+            output_time_major=False,
+            impute_finished=True,
+            maximum_iterations=None,
+            parallel_iterations=os.cpu_count(),
+            swap_memory=False,
+        )[0].rnn_output
 
         if mode == tf.estimator.ModeKeys.PREDICT:
 
