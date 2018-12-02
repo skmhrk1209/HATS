bd80931aa794a1194e29a1fc0bcbda351bee768f
Hiroki Sakuma <hirokisakuma@HirokinoMacBook-puro.local>
2018-12-02 21:57:45 +0900

    update

--- synth_main.py
+++ synth_main.py
@@ -14,7 +14,7 @@ from networks.attention_network import AttentionNetwork
 parser = argparse.ArgumentParser()
 parser.add_argument("--model_dir", type=str, default="synth_model", help="model directory")
 parser.add_argument('--filenames', type=str, nargs="+", default=["synth_train.tfrecord"], help="tfrecord filenames")
-parser.add_argument("--num_epochs", type=int, default=10, help="number of training epochs")
+parser.add_argument("--num_epochs", type=int, default=1, help="number of training epochs")
 parser.add_argument("--batch_size", type=int, default=128, help="batch size")
 parser.add_argument("--buffer_size", type=int, default=7000000, help="buffer size to shuffle dataset")
 parser.add_argument("--train", action="store_true", help="with training")
