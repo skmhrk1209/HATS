af092b5f5cfcf4e88cd28a21a97db5bace3911f8
Hiroki Sakuma <hirokisakuma@HirokinoMacBook-puro.local>
2018-12-09 23:50:35 +0900

    update

--- datasets/synth_text.py
+++ datasets/synth_text.py
@@ -82,7 +82,7 @@ def convert_dataset(input_directory, output_filename, *sequence_lengths):
         for filenames, texts, bounding_boxes in zip(dataset["imnames"][0],  dataset["txt"][0], dataset["wordBB"][0]):
 
             bounding_box_indices = [0] + list(accumulate([len(text.split()) for text in texts]))[:-1]
-            bounding_boxes = bounding_boxes.transpose(2, 1, 0)
+            bounding_boxes = bounding_boxes.transpose([2, 1, 0])
             bounding_boxes = [bounding_boxes[i] for i in bounding_box_indices]
 
             texts = [text for text, bounding_box in sorted(zip(texts, bounding_boxes), key=lambda t: t[1][0][::-1].tolist())]
