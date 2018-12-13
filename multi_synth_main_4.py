56169a2464a65381113c13bcbd01d04cf7f60106
Hiroki Sakuma <hirokisakuma@HirokinoMacBook-puro.local>
2018-12-13 10:38:48 +0900

    update

--- multi_synth_main_4.py
+++ multi_synth_main_4.py
@@ -13,7 +13,7 @@ from networks.residual_network import ResidualNetwork
 from networks.attention_network_2 import AttentionNetwork
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--model_dir", type=str, default="multi_synth_acnn_model_3", help="model directory")
+parser.add_argument("--model_dir", type=str, default="multi_synth_acnn_model_4", help="model directory")
 parser.add_argument('--filenames', type=str, nargs="+", default=["multi_synth_train.tfrecord"], help="tfrecord filenames")
 parser.add_argument("--num_epochs", type=int, default=10, help="number of training epochs")
 parser.add_argument("--batch_size", type=int, default=128, help="batch size")
@@ -53,8 +53,8 @@ def main(unused_argv):
                     AttrDict(filters=16, kernel_size=[3, 3], strides=[2, 2]),
                 ],
                 rnn_params=[
-                    AttrDict(sequence_length=4, num_units=512),
-                    AttrDict(sequence_length=10, num_units=512)
+                    AttrDict(sequence_length=4, num_units=128),
+                    AttrDict(sequence_length=10, num_units=128)
                 ],
                 channels_first=False
             ),
