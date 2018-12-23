52682a317b0e25a66b5435a51557fff247ff46dc
Hiroki Sakuma <hirokisakuma@HirokinoMacBook-puro.local>
2018-12-24 05:36:37 +0900

    update

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-gcc -std=c++14 make_dataset.cpp -o make_dataset \
+g++ -std=c++14 make_dataset.cpp -o make_dataset \
 -I/home/sakuma/local/include \
 -L/home/sakuma/local/lib \
 -lboost_system -lboost_filesystem -lboost_program_options -ljpeg -lpng
