ee41bd318d45d1cc736f69e610462141975e214e
Hiroki Sakuma <hirokisakuma@HirokinoMacBook-puro.local>
2018-12-24 14:52:37 +0900

    update

--- make_dataset.cpp
+++ make_dataset.cpp
@@ -13,6 +13,7 @@
 #include <random>
 #include <regex>
 #include <string>
+#include <thread>
 #include <utility>
 #include <vector>
 
@@ -41,45 +42,56 @@ int main(int argc, char* argv[]) {
     std::random_device seed;
     std::mt19937 engine(seed());
 
-    boost::progress_display progress_display(variables_map["num_data"].as<int>());
-    for (auto i = 0; i < variables_map["num_data"].as<int>(); ++i, ++progress_display) {
-        boost::gil::rgb8_image_t multi_image(variables_map["image_width"].as<int>(), variables_map["image_height"].as<int>());
-        boost::gil::fill_pixels(boost::gil::view(multi_image), boost::gil::rgb8_pixel_t(0, 0, 0));
-        std::vector<std::pair<std::string, boost::geometry::model::box<boost::geometry::model::d2::point_xy<int>>>> strings;
+    auto num_threads = std::thread::hardware_concurrency();
+    auto num_data = variables_map["num_data"].as<int>() / num_threads;
 
-        auto sequence_length = std::uniform_int_distribution<int>(1, variables_map["sequence_lengths"].as<std::vector<int>>()[0])(engine);
-        while (strings.size() < sequence_length) {
-            const auto& filename = filenames[std::uniform_int_distribution<int>(0, filenames.size() - 1)(engine)];
+    std::vector<std::thread> threads;
+    for (auto i = 0; i < num_threads; ++i) {
+        threads.emplace_back([&]() {
+            boost::progress_display progress_display(num_data);
+            for (auto j = num_data * i; j < num_data * (i + 1); ++j, ++progress_display) {
+                boost::gil::rgb8_image_t multi_image(variables_map["image_width"].as<int>(), variables_map["image_height"].as<int>());
+                boost::gil::fill_pixels(boost::gil::view(multi_image), boost::gil::rgb8_pixel_t(0, 0, 0));
+                std::vector<std::pair<std::string, boost::geometry::model::box<boost::geometry::model::d2::point_xy<int>>>> strings;
 
-            std::smatch match;
-            std::regex regex(R"([0-9]+_([0-9A-Za-z]*))");
-            if (!std::regex_match(filename.stem().string(), match, regex) || match[1].str().size() > variables_map["sequence_lengths"].as<std::vector<int>>()[1])
-                continue;
+                auto sequence_length = std::uniform_int_distribution<int>(1, variables_map["sequence_lengths"].as<std::vector<int>>()[0])(engine);
+                while (strings.size() < sequence_length) {
+                    const auto& filename = filenames[std::uniform_int_distribution<int>(0, filenames.size() - 1)(engine)];
 
-            boost::gil::rgb8_image_t image;
-            boost::gil::read_image(filename.string(), image, boost::gil::jpeg_tag());
+                    std::smatch match;
+                    std::regex regex(R"([0-9]+_([0-9A-Za-z]*))");
+                    if (!std::regex_match(filename.stem().string(), match, regex) || match[1].str().size() > variables_map["sequence_lengths"].as<std::vector<int>>()[1])
+                        continue;
 
-            if (image.height() > multi_image.height() || image.width() > multi_image.width()) continue;
+                    boost::gil::rgb8_image_t image;
+                    boost::gil::read_image(filename.string(), image, boost::gil::jpeg_tag());
 
-            auto dx = std::uniform_int_distribution<int>(0, multi_image.width() - image.width())(engine);
-            auto dy = std::uniform_int_distribution<int>(0, multi_image.height() - image.height())(engine);
-            boost::geometry::model::box<boost::geometry::model::d2::point_xy<int>> box(
-                boost::geometry::model::d2::point_xy<int>(dx, dy), boost::geometry::model::d2::point_xy<int>(dx + image.width(), dy + image.height()));
+                    if (image.height() > multi_image.height() || image.width() > multi_image.width()) continue;
 
-            if (std::all_of(strings.begin(), strings.end(), [&](const auto& string) { return boost::geometry::disjoint(string.second, box); })) {
-                boost::gil::copy_pixels(boost::gil::view(image), boost::gil::subimage_view(boost::gil::view(multi_image), dx, dy, image.width(), image.height()));
-                strings.emplace_back(match[1].str(), box);
+                    auto dx = std::uniform_int_distribution<int>(0, multi_image.width() - image.width())(engine);
+                    auto dy = std::uniform_int_distribution<int>(0, multi_image.height() - image.height())(engine);
+                    boost::geometry::model::box<boost::geometry::model::d2::point_xy<int>> box(
+                        boost::geometry::model::d2::point_xy<int>(dx, dy), boost::geometry::model::d2::point_xy<int>(dx + image.width(), dy + image.height()));
+
+                    if (std::all_of(strings.begin(), strings.end(), [&](const auto& string) { return boost::geometry::disjoint(string.second, box); })) {
+                        boost::gil::copy_pixels(boost::gil::view(image), boost::gil::subimage_view(boost::gil::view(multi_image), dx, dy, image.width(), image.height()));
+                        strings.emplace_back(match[1].str(), box);
+                    }
+                }
+
+                std::sort(strings.begin(), strings.end(), [](const auto& string1, const auto& string2) {
+                    return (string1.second.min_corner().y() < string2.second.min_corner().y()) ||
+                           ((string1.second.min_corner().y() == string2.second.min_corner().y()) && (string1.second.min_corner().x() < string2.second.min_corner().x()));
+                });
+
+                auto stem =
+                    std::accumulate(strings.begin(), strings.end(), std::to_string(j), [](const auto& acc, const auto& string) { return acc + "_" + string.first; });
+                boost::gil::write_view(variables_map["output_directory"].as<std::string>() + "/" + stem + ".jpg", boost::gil::view(multi_image), boost::gil::jpeg_tag());
             }
-        }
-
-        std::sort(strings.begin(), strings.end(), [](const auto& string1, const auto& string2) {
-            return (string1.second.min_corner().y() < string2.second.min_corner().y()) ||
-                   ((string1.second.min_corner().y() == string2.second.min_corner().y()) && (string1.second.min_corner().x() < string2.second.min_corner().x()));
         });
-
-        auto stem = std::accumulate(strings.begin(), strings.end(), std::to_string(i), [](const auto& acc, const auto& string) { return acc + "_" + string.first; });
-        boost::gil::write_view(variables_map["output_directory"].as<std::string>() + "/" + stem + ".jpg", boost::gil::view(multi_image), boost::gil::jpeg_tag());
     }
+
+    for (auto& thread : threads) thread.join();
 
     return 0;
 }
