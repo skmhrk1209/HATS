7c4485fc2e294c1f632f5cc1b460ea1e8f262436
Hiroki Sakuma <hirokisakuma@HirokinoMacBook-puro.local>
2018-12-24 15:46:46 +0900

    update

--- make_dataset.cpp
+++ make_dataset.cpp
@@ -26,7 +26,8 @@ int main(int argc, char* argv[]) {
         "image_width", boost::program_options::value<int>()->default_value(256), "width of image that will be generated")(
         "image_height", boost::program_options::value<int>()->default_value(256), "height of image that will be generated")(
         "sequence_lengths", boost::program_options::value<std::vector<int>>()->multitoken()->default_value({4, 10}, "4, 10"), "sequence lengths of texts")(
-        "num_data", boost::program_options::value<int>()->default_value(1000000), "number of data that will be generated");
+        "num_data", boost::program_options::value<int>()->default_value(1000000), "number of data that will be generated")(
+        "num_retries", boost::program_options::value<int>()->default_value(100), "number of retries for locating bounding box");
 
     boost::program_options::variables_map variables_map;
     boost::program_options::store(boost::program_options::parse_command_line(argc, argv, options_description), variables_map);
@@ -55,29 +56,38 @@ int main(int argc, char* argv[]) {
                 std::vector<std::pair<std::string, boost::geometry::model::box<boost::geometry::model::d2::point_xy<int>>>> strings;
 
                 auto sequence_length = std::uniform_int_distribution<int>(1, variables_map["sequence_lengths"].as<std::vector<int>>()[0])(engine);
-                while (strings.size() < sequence_length) {
-                    const auto& filename = filenames[std::uniform_int_distribution<int>(0, filenames.size() - 1)(engine)];
+                if (![&]() {
+                        for (auto k = 0; k < variables_map["num_retries"].as<int>(); ++k) {
+                            const auto& filename = filenames[std::uniform_int_distribution<int>(0, filenames.size() - 1)(engine)];
 
-                    std::smatch match;
-                    std::regex regex(R"([0-9]+_([0-9A-Za-z]*))");
-                    if (!std::regex_match(filename.stem().string(), match, regex) || match[1].str().size() > variables_map["sequence_lengths"].as<std::vector<int>>()[1])
-                        continue;
+                            std::smatch match;
+                            std::regex regex(R"([0-9]+_([0-9A-Za-z]*))");
+                            if (!std::regex_match(filename.stem().string(), match, regex) ||
+                                match[1].str().size() > variables_map["sequence_lengths"].as<std::vector<int>>()[1])
+                                continue;
 
-                    boost::gil::rgb8_image_t image;
-                    boost::gil::read_image(filename.string(), image, boost::gil::jpeg_tag());
+                            boost::gil::rgb8_image_t image;
+                            boost::gil::read_image(filename.string(), image, boost::gil::jpeg_tag());
 
-                    if (image.height() > multi_image.height() || image.width() > multi_image.width()) continue;
+                            if (image.height() > multi_image.height() || image.width() > multi_image.width()) continue;
 
-                    auto dx = std::uniform_int_distribution<int>(0, multi_image.width() - image.width())(engine);
-                    auto dy = std::uniform_int_distribution<int>(0, multi_image.height() - image.height())(engine);
-                    boost::geometry::model::box<boost::geometry::model::d2::point_xy<int>> box(
-                        boost::geometry::model::d2::point_xy<int>(dx, dy), boost::geometry::model::d2::point_xy<int>(dx + image.width(), dy + image.height()));
+                            auto dx = std::uniform_int_distribution<int>(0, multi_image.width() - image.width())(engine);
+                            auto dy = std::uniform_int_distribution<int>(0, multi_image.height() - image.height())(engine);
+                            boost::geometry::model::box<boost::geometry::model::d2::point_xy<int>> box(
+                                boost::geometry::model::d2::point_xy<int>(dx, dy), boost::geometry::model::d2::point_xy<int>(dx + image.width(), dy + image.height()));
 
-                    if (std::all_of(strings.begin(), strings.end(), [&](const auto& string) { return boost::geometry::disjoint(string.second, box); })) {
-                        boost::gil::copy_pixels(boost::gil::view(image), boost::gil::subimage_view(boost::gil::view(multi_image), dx, dy, image.width(), image.height()));
-                        strings.emplace_back(match[1].str(), box);
-                    }
-                }
+                            if (std::all_of(strings.begin(), strings.end(), [&](const auto& string) { return boost::geometry::disjoint(string.second, box); })) {
+                                boost::gil::copy_pixels(boost::gil::view(image),
+                                                        boost::gil::subimage_view(boost::gil::view(multi_image), dx, dy, image.width(), image.height()));
+                                strings.emplace_back(match[1].str(), box);
+
+                                if (strings.size() == sequence_length) return true;
+                            }
+                        }
+
+                        return false;
+                    }())
+                    continue;
 
                 std::sort(strings.begin(), strings.end(), [](const auto& string1, const auto& string2) {
                     return (string1.second.min_corner().y() < string2.second.min_corner().y()) ||
