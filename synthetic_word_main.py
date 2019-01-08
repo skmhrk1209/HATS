9e3cb8e69fe34c4400e81cf617a12d81ed77bb10
Hiroki Sakuma <hirokisakuma@HirokinoMacBook-puro.local>
2019-01-08 19:02:23 +0900

    update

--- synthetic_word_main.py
+++ synthetic_word_main.py
@@ -8,8 +8,8 @@ from networks.residual_network import ResidualNetwork
 from networks.attention_network import AttentionNetwork
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--model_dir", type=str, default="synth_acnn_model", help="model directory")
-parser.add_argument('--filenames', type=str, nargs="+", default=["synth_train.tfrecord"], help="tfrecord filenames")
+parser.add_argument("--model_dir", type=str, default="synthetic_word_acnn_model", help="model directory")
+parser.add_argument('--filenames', type=str, nargs="+", default=["synthetic_word_train.tfrecord"], help="tfrecord filenames")
 parser.add_argument("--num_epochs", type=int, default=10, help="number of training epochs")
 parser.add_argument("--batch_size", type=int, default=128, help="batch size")
 parser.add_argument("--buffer_size", type=int, default=7000000, help="buffer size to shuffle dataset")
