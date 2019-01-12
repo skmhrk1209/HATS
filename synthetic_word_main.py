4bc29a5e6f95a841340370df5d1ead6085003b1b
Hiroki Sakuma <hirokisakuma@HirokinoMacBook-puro.local>
2019-01-12 21:03:56 +0900

    update

--- synthetic_word_main.py
+++ synthetic_word_main.py
@@ -142,6 +142,7 @@ def main(unused_argv):
             attention_maps = map_innermost_list(sum, attention_maps)
             attention_maps = map_innermost_list(sum, attention_maps)
 
+            print(type(attention_maps))
             map_innermost_list(lambda l: print(sum(l)), attention_maps)
 
             if args.data_format == "channels_first":
