c10a40948127ea14bee1a1a9a36687e401e20549
Hiroki Sakuma <hirokisakuma@HirokinoMacBook-puro.local>
2019-01-14 07:57:12 +0900

    update

--- word_recognition_main.py
+++ word_recognition_main.py
@@ -11,6 +11,7 @@ from networks.attention_network import AttentionNetwork
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--model_dir", type=str, default="word_recognition_acnn_model", help="model directory")
+parser.add_argument("--pretrained_model_dir", type=str, default="synthetic_word_acnn_model", help="model directory")
 parser.add_argument('--filenames', type=str, nargs="+", default=["word_recognition_train.tfrecord"], help="tfrecord filenames")
 parser.add_argument("--num_epochs", type=int, default=1000, help="number of training epochs")
 parser.add_argument("--batch_size", type=int, default=128, help="batch size")
@@ -61,7 +62,7 @@ def main(unused_argv):
                 beta1=0.9,
                 beta2=0.999
             ),
-            pretrained_model="synthetic_word_acnn_model"
+            pretrained_model_dir=args.pretrained_model_dir
         )(features, labels, mode),
         model_dir=args.model_dir,
         config=tf.estimator.RunConfig().replace(
