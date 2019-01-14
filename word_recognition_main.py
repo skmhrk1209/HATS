2a5048472f41ae0b509ef8c1a722b2accc7c8d2d
Hiroki Sakuma <hirokisakuma@HirokinoMacBook-puro.local>
2019-01-14 16:37:31 +0900

    update

--- word_recognition_main.py
+++ word_recognition_main.py
@@ -63,7 +63,7 @@ def main(unused_argv):
                 beta2=0.999
             ),
             pretrained_model_dir=args.pretrained_model_dir
-        )(features["image"], labels, mode),
+        )(features["images"] if isinstance(features, dict) else features, labels, mode),
         model_dir=args.model_dir,
         config=tf.estimator.RunConfig().replace(
             session_config=tf.ConfigProto(
