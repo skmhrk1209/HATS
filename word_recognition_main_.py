4120a5be3cf089a34184d6331d4febf6cce5ad97
Hiroki Sakuma <hirokisakuma@HirokinoMacBook-puro.local>
2019-01-14 19:03:14 +0900

    update

--- word_recognition_main_.py
+++ word_recognition_main_.py
@@ -46,8 +46,8 @@ def main(unused_argv):
             ),
             attention_network=AttentionNetwork(
                 conv_params=[
-                    AttrDict(filters=4, kernel_size=[9, 9], strides=[2, 2]),
-                    AttrDict(filters=4, kernel_size=[9, 9], strides=[2, 2]),
+                    AttrDict(filters=16, kernel_size=[9, 9], strides=[2, 2]),
+                    AttrDict(filters=16, kernel_size=[9, 9], strides=[2, 2]),
                 ],
                 deconv_params=[
                     AttrDict(filters=16, kernel_size=[3, 3], strides=[2, 2]),
