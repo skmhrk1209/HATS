c529276ce1d79af5c9527dc22e5ede345349d68a
Hiroki Sakuma <hirokisakuma@HirokinoMacBook-puro.local>
2019-01-14 16:32:43 +0900

    update

--- word_recognition_main.py
+++ word_recognition_main.py
@@ -117,7 +117,7 @@ def main(unused_argv):
 
         filenames = sorted(
             iterable=glob.glob("evaluation_dataset/*.png"),
-            key=lambda filename: int(os.path.splitext(filename))[0].split("_")[1]
+            key=lambda filename: int(os.path.splitext(filename)[0].split("_")[1])
         )
 
         images = np.array([np.transpose(
