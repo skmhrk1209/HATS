437dd833429858ac364f60165269a6bdb110a864
Hiroki Sakuma <hirokisakuma@HirokinoMacBook-puro.local>
2019-01-17 05:28:15 +0900

    update

---
+++ models/stts.py
@@ -0,0 +1,129 @@
+import tensorflow as tf
+import numpy as np
+import metrics
+from algorithms import *
+
+
+class STTS(object):
+
+    def __init__(self, backbone_network, spatial_transformer_network,
+                 num_classes, data_format, hyper_params, pretrained_model_dir=None):
+
+        self.backbone_network = backbone_network
+        self.spatial_transformer_network = spatial_transformer_network
+        self.num_classes = num_classes
+        self.data_format = data_format
+        self.hyper_params = hyper_params
+        self.pretrained_model_dir = pretrained_model_dir
+
+    def __call__(self, images, labels, mode):
+
+        feature_maps = self.backbone_network(
+            inputs=images,
+            training=mode == tf.estimator.ModeKeys.TRAIN
+        )
+
+        feature_maps = self.spatial_transformer_network(
+            inputs=feature_maps,
+            training=mode == tf.estimator.ModeKeys.TRAIN
+        )
+
+        feature_vectors = map_innermost_element(
+            function=lambda feature_maps: tf.layers.flatten(feature_maps),
+            sequence=feature_maps
+        )
+
+        logits = map_innermost_element(
+            function=lambda feature_vectors: tf.layers.dense(
+                inputs=feature_vectors,
+                units=self.num_classes,
+                name="logits",
+                reuse=tf.AUTO_REUSE
+            ),
+            sequence=feature_vectors
+        )
+
+        predictions = map_innermost_element(
+            function=lambda logits: tf.argmax(
+                input=logits,
+                axis=-1,
+                output_type=tf.int32
+            ),
+            sequence=logits
+        )
+
+        if mode == tf.estimator.ModeKeys.PREDICT:
+
+            while isinstance(predictions, list):
+
+                predictions = map_innermost_list(
+                    function=lambda predictions: tf.stack(predictions, axis=1),
+                    sequence=predictions
+                )
+
+            return tf.estimator.EstimatorSpec(
+                mode=mode,
+                predictions=dict(
+                    images=images,
+                    attention_maps=attention_maps,
+                    predictions=predictions
+                )
+            )
+
+        while all(flatten_innermost_element(map_innermost_element(lambda labels: len(labels.shape) > 1, labels))):
+
+            labels = map_innermost_element(
+                function=lambda labels: tf.unstack(labels, axis=1),
+                sequence=labels
+            )
+
+        loss = tf.reduce_mean(map_innermost_element(
+            function=lambda labels_logits: tf.losses.sparse_softmax_cross_entropy(*labels_logits),
+            sequence=zip_innermost_element(labels, logits)
+        ))
+
+        labels = tf.concat(flatten_innermost_element(map_innermost_list(
+            function=lambda labels: tf.stack(labels, axis=1),
+            sequence=labels
+        )), axis=0)
+
+        logits = tf.concat(flatten_innermost_element(map_innermost_list(
+            function=lambda logits: tf.stack(logits, axis=1),
+            sequence=logits
+        )), axis=0)
+
+        error_rate = metrics.normalized_edit_distance(labels, logits)
+
+        # ==========================================================================================
+        tf.identity(error_rate[0], "error_rate")
+        tf.summary.scalar("error_rate", error_rate[1])
+        # ==========================================================================================
+
+        if mode == tf.estimator.ModeKeys.TRAIN:
+
+            with tf.control_dependencies(tf.get_collection(tf.GraphKeys.UPDATE_OPS)):
+
+                optimizer = tf.train.AdamOptimizer(
+                    learning_rate=self.hyper_params.learning_rate,
+                    beta1=self.hyper_params.beta1,
+                    beta2=self.hyper_params.beta2
+                )
+
+                train_op = optimizer.minimize(
+                    loss=loss,
+                    global_step=tf.train.get_global_step()
+                )
+
+            return tf.estimator.EstimatorSpec(
+                mode=mode,
+                loss=loss,
+                train_op=train_op
+            )
+
+        if mode == tf.estimator.ModeKeys.EVAL:
+
+            return tf.estimator.EstimatorSpec(
+                mode=mode,
+                loss=loss,
+                eval_metric_ops=dict(error_rate=error_rate)
+            )
---
+++ networks/stn.py
@@ -0,0 +1,62 @@
+import tensorflow as tf
+import numpy as np
+from . import ops
+from algorithms import *
+from itertools import *
+from spatial_transformer import transformer
+
+
+class STN(object):
+
+    def __init__(self, rnn_params, out_size, data_format):
+
+        self.rnn_params = rnn_params
+        self.out_size = out_size
+        self.data_format = data_format
+
+    def __call__(self, inputs, training, name="stn", reuse=None):
+
+        feature_maps = inputs
+
+        for i, rnn_param in enumerate(self.rnn_params[:-1]):
+
+            with tf.variable_scope("rnn_block_{}".format(i)):
+
+                multi_lstm_cell = tf.nn.rnn_cell.MultiRNNCell([
+                    tf.nn.rnn_cell.LSTMCell(
+                        num_units=num_units,
+                        use_peepholes=True,
+                        activation=tf.nn.tanh,
+                        initializer=tf.variance_scaling_initializer(
+                            scale=1.0,
+                            mode="fan_avg",
+                            distribution="normal"
+                        )
+                    ) for num_units in rnn_param.num_units
+                ])
+
+                inputs = map_innermost_element(
+                    function=lambda inputs: list(zip(
+                        [inputs] * rnn_param.sequence_length,
+                        tf.nn.static_rnn(
+                            cell=multi_lstm_cell,
+                            inputs=[tf.layers.flatten(inputs)] * rnn_param.sequence_length,
+                            initial_state=multi_lstm_cell.zero_state(
+                                batch_size=tf.shape(inputs)[0],
+                                dtype=tf.float32
+                            )
+                        )[0]
+                    )),
+                    sequence=inputs
+                )
+
+                inputs = map_innermost_element(
+                    function=lambda inputs_theta: transformer(
+                        U=inputs_theta[0],
+                        theta=inputs_theta[1],
+                        out_size=self.out_size
+                    ),
+                    sequence=inputs
+                )
+
+            return inputs
