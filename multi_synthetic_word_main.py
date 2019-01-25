da749650646be845c08e17334458d48f5aeac16a
Hiroki Sakuma <hirokisakuma@HirokinoMacBook-puro.local>
2019-01-25 20:09:27 +0900

    update

--- multi_synthetic_word_main.py
+++ multi_synthetic_word_main.py
@@ -145,7 +145,8 @@ def main(unused_argv):
                     attention_map = img.scale(attention_map, attention_map.min(), attention_map.max(), 0.0, 1.0)
                     attention_map = cv2.resize(attention_map, image.shape[:-1])
                     bounding_box = img.search_bounding_box(attention_map, 0.5, 1.0)
-                    #cv2.rectangle(image, bounding_box[0][::-1], bounding_box[1][::-1], (0, 0, 255), 2)
+                    print(image)
+                    image = cv2.rectangle(image, bounding_box[0][::-1], bounding_box[1][::-1], (0, 0, 255), 2)
 
             image = img.scale(image, 0.0, 1.0, 0.0, 255.0)
             cv2.imwrite("outputs/attention_map_{}.jpg".format(i), image)
