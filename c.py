fb1570b9b2fda887c915c18adf010c28bae9d935
Hiroki Sakuma <hirokisakuma@HirokinoMacBook-puro.local>
2019-01-29 23:05:34 +0900

    update

--- c.py
+++ c.py
@@ -9,5 +9,5 @@ dir = "../data/synth90k/"
 with open(os.path.join(dir, "annotation_test.txt")) as f:
     for line in tqdm(f):
         path = os.path.join(dir, line.split()[0])
-        if cv2.imread(path) is None:
+        if os.path.exists(path) and (cv2.imread(path) is None):
             print(path)
