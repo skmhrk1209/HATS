9e9b8ac201ee082f75d3d11128ca5e771c49b3ee
Hiroki Sakuma <hirokisakuma@HirokinoMacBook-puro.local>
2019-01-31 07:12:08 +0900

    update

--- model.py
+++ model.py
@@ -110,8 +110,6 @@ class HATS(object):
         edit_distance = metrics.edit_distance(labels, logits, normalize=True)
         sequence_accuracy = metrics.sequence_accuracy(labels, logits)
 
-        print(edit_distance[0].shape)
-
         print("num params: {}".format(sum([
             np.prod(variable.get_shape().as_list())
             for variable in tf.trainable_variables()
