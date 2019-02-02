cd95984f5fc9951abfcdf549044212671790444b
Hiroki Sakuma <hirokisakuma@HirokinoMacBook-puro.local>
2019-02-02 17:30:26 +0900

    update

--- chars74k_main_3.py
+++ chars74k_main_3.py
@@ -1,31 +1,27 @@
 # =============================================================
 # dataset details
-# dataset: multi synth90k
-# download: made by make_multi_synth90k.cpp
-# train: 900000
-# val: 50000
-# test: 50000
-# max num chars: 10
+# dataset: chars74k
+# download: http://www.ee.surrey.ac.uk/CVSSP/demos/chars74k/
+# train: 11252
+# test: 1251
 # classes: [0-9A-Z](case-insensitive)
-# word accuracy: 0.863 (100000 steps)
-# edit distance: 0.033 (100000 steps)
 # =============================================================
+
 
 import tensorflow as tf
 import tensorflow_hub as hub
 import argparse
 from attrdict import AttrDict
 from dataset import Dataset
-from models.hats import HATS
-from networks.han import HAN
+from models.classifier import Classifier
 from networks.resnet_2 import ResNet
 from algorithms import *
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--model_dir", type=str, default="multi_synth90k_hats_model_5", help="model directory")
+parser.add_argument("--model_dir", type=str, default="chars74k_resnet_model_3", help="model directory")
 parser.add_argument("--pretrained_model_dir", type=str, default="", help="pretrained model directory")
-parser.add_argument('--filenames', type=str, nargs="+", default=["multi_synth90k_train.tfrecord"], help="tfrecord filenames")
-parser.add_argument("--num_epochs", type=int, default=10, help="number of training epochs")
+parser.add_argument('--filenames', type=str, nargs="+", default=["chars74k_train.tfrecord"], help="tfrecord filenames")
+parser.add_argument("--num_epochs", type=int, default=100, help="number of training epochs")
 parser.add_argument("--batch_size", type=int, default=128, help="batch size")
 parser.add_argument("--data_format", type=str, default="channels_first", help="data format")
 parser.add_argument("--steps", type=int, default=None, help="number of training epochs")
@@ -43,7 +39,7 @@ tf.logging.set_verbosity(tf.logging.INFO)
 def main(unused_argv):
 
     classifier = tf.estimator.Estimator(
-        model_fn=lambda features, labels, mode: HATS(
+        model_fn=lambda features, labels, mode: Classifier(
             backbone_network=ResNet(
                 conv_param=AttrDict(filters=64, kernel_size=[7, 7], strides=[2, 2]),
                 pool_param=AttrDict(pool_size=[3, 3], strides=[2, 2]),
@@ -53,29 +49,11 @@ def main(unused_argv):
                     AttrDict(filters=256, strides=[2, 2], blocks=2),
                     AttrDict(filters=512, strides=[2, 2], blocks=2),
                 ],
-                data_format=args.data_format,
-                pretrained_model_dir=args.pretrained_model_dir,
-                pretrained_model_scope="resnet"
-            ),
-            attention_network=HAN(
-                conv_params=[
-                    AttrDict(filters=16, kernel_size=[3, 3], strides=[2, 2]),
-                    AttrDict(filters=16, kernel_size=[3, 3], strides=[2, 2]),
-                ],
-                deconv_params=[
-                    AttrDict(filters=16, kernel_size=[3, 3], strides=[2, 2]),
-                    AttrDict(filters=16, kernel_size=[3, 3], strides=[2, 2]),
-                ],
-                rnn_params=[
-                    AttrDict(sequence_length=5, num_units=256),
-                    AttrDict(sequence_length=10, num_units=256),
-                ],
                 data_format=args.data_format
             ),
-            num_classes=63,
+            num_classes=37,
             data_format=args.data_format,
             hyper_params=AttrDict(
-                attention_decay=1e-4,
                 learning_rate=0.001,
                 beta1=0.9,
                 beta2=0.999
@@ -100,10 +78,10 @@ def main(unused_argv):
                 filenames=args.filenames,
                 num_epochs=args.num_epochs,
                 batch_size=args.batch_size,
-                sequence_lengths=[5, 10],
-                image_size=[256, 256],
+                sequence_lengths=[],
+                image_size=[128, 128],
                 data_format=args.data_format,
-                encoding="jpeg"
+                encoding="png"
             ),
             steps=args.steps,
             max_steps=args.max_steps
@@ -116,51 +94,14 @@ def main(unused_argv):
                 filenames=args.filenames,
                 num_epochs=1,
                 batch_size=args.batch_size,
-                sequence_lengths=[5, 10],
-                image_size=[256, 256],
+                sequence_lengths=[],
+                image_size=[128, 128],
                 data_format=args.data_format,
-                encoding="jpeg"
+                encoding="png"
             )
         )
 
         print(eval_results)
-
-    if args.predict:
-
-        import cv2
-        import itertools
-
-        predict_results = classifier.predict(
-            input_fn=Dataset(
-                filenames=args.filenames,
-                num_epochs=1,
-                batch_size=args.batch_size,
-                sequence_lengths=[5, 10],
-                image_size=[256, 256],
-                data_format=args.data_format,
-                encoding="jpeg"
-            )
-        )
-
-        for i, predict_result in enumerate(itertools.islice(predict_results, 100)):
-
-            image = predict_result["images"]
-            attention_maps = predict_result["attention_maps"]
-
-            if args.data_format == "channels_first":
-                image = np.transpose(image, [1, 2, 0])
-                attention_maps = np.transpose(attention_maps, [0, 1, 3, 4, 2])
-
-            for attention_maps_ in attention_maps:
-
-                for attention_map in attention_maps_:
-
-                    attention_map = (attention_map - attention_map.min()) / (attention_map.max() - attention_map.min())
-                    attention_map[attention_map < 0.5] = 0.0
-                    attention_map = cv2.resize(attention_map, image.shape[:-1])
-                    image[:, :, -1] += attention_map
-
-            cv2.imwrite("outputs/attention_map_{}.jpg".format(i), image * 255.)
 
 
 if __name__ == "__main__":
---
+++ models/hats_2.py
@@ -0,0 +1,223 @@
+import tensorflow as tf
+import numpy as np
+import metrics
+from algorithms import *
+
+
+class HATS(object):
+
+    def __init__(self, backbone_network, attention_network, num_classes, data_format, hyper_params):
+
+        self.backbone_network = backbone_network
+        self.attention_network = attention_network
+        self.num_classes = num_classes
+        self.data_format = data_format
+        self.hyper_params = hyper_params
+
+    def __call__(self, images, labels, mode):
+
+        feature_maps = self.backbone_network(
+            inputs=images,
+            training=mode == tf.estimator.ModeKeys.TRAIN
+        )
+
+        inputs = feature_maps.pop()
+
+        while feature_maps:
+
+            shape = feature_maps[-1].get_shape().as_list()
+
+            inputs = ops.bilinear_upsampling(
+                inputs=inputs,
+                size=shape[2:] if self.data_format == "channels_first" else shape[1:-1],
+                data_format=self.data_format
+            )
+
+            inputs = tf.layers.conv2d(
+                inputs=inputs,
+                filters=shape[1] if self.data_format == "channels_first" else shape[-1],
+                kernel_size=[1, 1],
+                strides=[1, 1],
+                padding="same",
+                data_format=self.data_format,
+                use_bias=False,
+                kernel_initializer=tf.variance_scaling_initializer(
+                        scale=2.0,
+                        mode="fan_in",
+                        distribution="normal"
+                )
+            )
+
+            inputs = ops.batch_normalization(
+                inputs=inputs,
+                data_format=self.data_format,
+                training=training
+            )
+
+            inputs = tf.nn.relu(inputs)
+
+            inputs += feature_maps.pop()
+
+        attention_maps = self.attention_network(
+            inputs=feature_maps,
+            training=mode == tf.estimator.ModeKeys.TRAIN
+        )
+
+        def spatial_flatten(inputs, data_format):
+
+            inputs_shape = inputs.get_shape().as_list()
+            outputs_shape = ([-1, inputs_shape[1], np.prod(inputs_shape[2:])] if data_format == "channels_first" else
+                             [-1, np.prod(inputs_shape[1:-1]), inputs_shape[-1]])
+
+            return tf.reshape(inputs, outputs_shape)
+
+        feature_vectors = map_innermost_element(
+            function=lambda attention_maps: tf.layers.flatten(tf.matmul(
+                a=spatial_flatten(feature_maps, self.data_format),
+                b=spatial_flatten(attention_maps, self.data_format),
+                transpose_a=False if self.data_format == "channels_first" else True,
+                transpose_b=True if self.data_format == "channels_first" else False
+            )),
+            sequence=attention_maps
+        )
+
+        logits = map_innermost_element(
+            function=lambda feature_vectors: tf.layers.dense(
+                inputs=feature_vectors,
+                units=self.num_classes,
+                name="logits",
+                reuse=tf.AUTO_REUSE
+            ),
+            sequence=feature_vectors
+        )
+
+        predictions = map_innermost_element(
+            function=lambda logits: tf.argmax(logits, axis=-1),
+            sequence=logits
+        )
+
+        attention_maps = map_innermost_element(
+            function=lambda attention_maps: tf.reduce_sum(
+                input_tensor=attention_maps,
+                axis=1 if self.data_format == "channels_first" else 3,
+                keep_dims=True
+            ),
+            sequence=attention_maps
+        )
+
+        if mode == tf.estimator.ModeKeys.PREDICT:
+
+            while isinstance(attention_maps, list):
+
+                attention_maps = map_innermost_list(
+                    function=lambda attention_maps: tf.stack(attention_maps, axis=1),
+                    sequence=attention_maps
+                )
+
+            while isinstance(predictions, list):
+
+                predictions = map_innermost_list(
+                    function=lambda predictions: tf.stack(predictions, axis=1),
+                    sequence=predictions
+                )
+
+            return tf.estimator.EstimatorSpec(
+                mode=mode,
+                predictions=dict(
+                    images=images,
+                    attention_maps=attention_maps,
+                    predictions=predictions
+                )
+            )
+
+        while all(flatten_innermost_element(map_innermost_element(lambda labels: len(labels.shape) > 1, labels))):
+
+            labels = map_innermost_element(
+                function=lambda labels: tf.unstack(labels, axis=1),
+                sequence=labels
+            )
+
+        loss = tf.reduce_mean(map_innermost_element(
+            function=lambda labels_logits: tf.losses.sparse_softmax_cross_entropy(*labels_logits),
+            sequence=zip_innermost_element(labels, logits)
+        ))
+
+        loss += tf.reduce_mean(map_innermost_element(
+            function=lambda attention_maps: tf.reduce_mean(tf.reduce_sum(attention_maps, axis=[1, 2, 3])),
+            sequence=attention_maps
+        )) * self.hyper_params.attention_decay
+
+        labels = tf.concat(flatten_innermost_element(map_innermost_list(
+            function=lambda labels: tf.stack(labels, axis=1),
+            sequence=labels
+        )), axis=0)
+
+        logits = tf.concat(flatten_innermost_element(map_innermost_list(
+            function=lambda logits: tf.stack(logits, axis=1),
+            sequence=logits
+        )), axis=0)
+
+        # ==========================================================================================
+        if self.data_format == "channels_first":
+
+            images = tf.transpose(images, [0, 2, 3, 1])
+
+            attention_maps = map_innermost_element(
+                function=lambda attention_maps: tf.transpose(attention_maps, [0, 2, 3, 1]),
+                sequence=attention_maps
+            )
+
+        tf.summary.image("images", images, max_outputs=2)
+
+        map_innermost_element(
+            function=lambda indices_attention_maps: tf.summary.image(
+                name="attention_maps_{}".format("_".join(map(str, indices_attention_maps[0]))),
+                tensor=indices_attention_maps[1],
+                max_outputs=2
+            ),
+            sequence=enumerate_innermost_element(attention_maps)
+        )
+        # ==========================================================================================
+
+        if mode == tf.estimator.ModeKeys.TRAIN:
+
+            with tf.control_dependencies(tf.get_collection(tf.GraphKeys.UPDATE_OPS)):
+
+                optimizer = tf.train.AdamOptimizer(
+                    learning_rate=self.hyper_params.learning_rate,
+                    beta1=self.hyper_params.beta1,
+                    beta2=self.hyper_params.beta2
+                )
+
+                train_op = optimizer.minimize(
+                    loss=loss,
+                    global_step=tf.train.get_global_step()
+                )
+
+            return tf.estimator.EstimatorSpec(
+                mode=mode,
+                loss=loss,
+                train_op=train_op
+            )
+
+        if mode == tf.estimator.ModeKeys.EVAL:
+
+            accuracy = metrics.sequence_accuracy(
+                labels=labels,
+                logits=logits
+            )
+
+            edit_distance = metrics.edit_distance(
+                labels=labels,
+                logits=logits,
+                normalize=True
+            )
+
+            return tf.estimator.EstimatorSpec(
+                mode=mode,
+                loss=loss,
+                eval_metric_ops=dict(
+                    accuracy=accuracy,
+                    edit_distance=edit_distance
+                )
+            )
