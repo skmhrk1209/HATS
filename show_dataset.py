f4376125596f6d086b56c5933928cb80380279bd
Hiroki Sakuma <hirokisakuma@HirokinoMacBook-puro.local>
2019-02-03 21:06:13 +0900

    update

--- show_dataset.py
+++ show_dataset.py
@@ -6,7 +6,7 @@ class_ids = {}
 class_ids.update({chr(j): i for i, j in enumerate(range(ord("0"), ord("9") + 1), 0)})
 class_ids.update({chr(j): i for i, j in enumerate(range(ord("A"), ord("Z") + 1), class_ids["9"] + 1)})
 class_ids.update({"": max(class_ids.values()) + 1})
-class_names = dict(map(lambda item: item[::-1], class_ids.items()))
+class_names = {class_id: class_name for class_name, class_id in class_ids}
 
 for record in tf.python_io.tf_record_iterator(sys.argv[1]):
 
@@ -14,7 +14,7 @@ for record in tf.python_io.tf_record_iterator(sys.argv[1]):
     example.ParseFromString(record)
 
     image = cv2.imread(example.features.feature["path"].bytes_list.value[0].decode())
-    label = "".join(list(map(lambda class_id: class_names[class_id], example.features.feature["label"].int64_list.value)))
+    label = "".join([class_names[class_id] for class_id in example.features.feature["label"].int64_list.value])
 
     print(label)
     cv2.imshow("image", image)
