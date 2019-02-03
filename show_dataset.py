9af08e8d4d0ed90dbdd7a1e1cd455422d39ff08f
Hiroki Sakuma <hirokisakuma@HirokinoMacBook-puro.local>
2019-02-03 20:57:46 +0900

    update

--- show_dataset.py
+++ show_dataset.py
@@ -2,13 +2,19 @@ import tensorflow as tf
 import cv2
 import sys
 
+class_ids = {}
+class_ids.update({chr(j): i for i, j in enumerate(range(ord("0"), ord("9") + 1), 0)})
+class_ids.update({chr(j): i for i, j in enumerate(range(ord("A"), ord("Z") + 1), class_ids["9"] + 1)})
+class_ids.update({"": max(class_ids.values()) + 1})
+class_names = dict(map(lambda x: x[::-1], class_ids.items()))
+
 for record in tf.python_io.tf_record_iterator(sys.argv[1]):
 
     example = tf.train.Example()
     example.ParseFromString(record)
 
     path = example.features.feature["path"].bytes_list.value[0].decode()
-    label = example.features.feature["label"].bytes_list.value[0].decode()
+    label = str(map(lambda class_id: class_names[class_id], example.features.feature["label"].bytes_list.value))
 
     print(label)
     cv2.imshow("image", cv2.imread(path))
