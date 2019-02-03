8aaab5365eccb521b5473c49ac4b4c1286459483
Hiroki Sakuma <hirokisakuma@HirokinoMacBook-puro.local>
2019-02-03 20:51:12 +0900

    update

---
+++ show_dataset.py
@@ -0,0 +1,16 @@
+import tensorflow as tf
+import cv2
+import sys
+
+for record in tf.python_io.tf_record_iterator(sys.argv[1]):
+
+    example = tf.train.Example()
+    example.ParseFromString(record)
+
+    path = example.features.feature["path"].bytes_list.value[0].decode()
+    label = example.features.feature["label"].bytes_list.value[0].decode()
+
+    print(label)
+    cv2.imshow("image", cv2.imread(path))
+    if cv2.waitKey() == ord("q"):
+        break
