f49ef3c3dd1a08df72b3f7f67fc9f44d49636990
Hiroki Sakuma <hirokisakuma@HirokinoMacBook-puro.local>
2019-02-07 04:02:16 +0900

    update

--- multi_synth90k_main_.py
+++ multi_synth90k_main_.py
@@ -1,16 +1,14 @@
 # =============================================================
 # dataset details
-# dataset: multi-synth90k
+# dataset: multi synth90k
 # download: made by make_multi_synth90k.cpp
 # train: 800000
 # val: 100000
 # test: 100000
 # max num chars: 10
 # classes: [0-9A-Z](case-insensitive)
-# word accuracy:
-# edit distance:
-# pretrained model: chars74k classifier
-# max steps: 100000 batch size: 128
+# word accuracy: 0.863 (100000 steps)
+# edit distance: 0.033 (100000 steps)
 # =============================================================
 
 import tensorflow as tf
@@ -27,7 +25,7 @@ parser.add_argument("--model_dir", type=str, default="multi_synth90k_hats", help
 parser.add_argument("--pretrained_model_dir", type=str, default="", help="pretrained model directory")
 parser.add_argument('--filenames', type=str, nargs="+", default=["multi_synth90k_train.tfrecord"], help="tfrecord filenames")
 parser.add_argument("--num_epochs", type=int, default=10, help="number of training epochs")
-parser.add_argument("--batch_size", type=int, default=128, help="batch size")
+parser.add_argument("--batch_size", type=int, default=64, help="batch size")
 parser.add_argument("--data_format", type=str, default="channels_first", help="data format")
 parser.add_argument("--steps", type=int, default=None, help="number of training epochs")
 parser.add_argument("--max_steps", type=int, default=None, help="maximum number of training epochs")
@@ -47,9 +45,9 @@ def main(unused_argv):
         model_fn=lambda features, labels, mode: HATS(
             backbone_network=PyramidResNet(
                 conv_param=AttrDict(filters=64, kernel_size=[7, 7], strides=[2, 2]),
-                pool_param=None,
+                pool_param=AttrDict(pool_size=[3, 3], strides=[2, 2]),
                 residual_params=[
-                    AttrDict(filters=64, strides=[2, 2], blocks=2),
+                    AttrDict(filters=64, strides=[1, 1], blocks=2),
                     AttrDict(filters=128, strides=[2, 2], blocks=2),
                     AttrDict(filters=256, strides=[2, 2], blocks=2),
                     AttrDict(filters=512, strides=[2, 2], blocks=2),
@@ -60,16 +58,16 @@ def main(unused_argv):
             ),
             attention_network=AttentionNetwork(
                 conv_params=[
-                    AttrDict(filters=8, kernel_size=[3, 3], strides=[2, 2]),
-                    AttrDict(filters=8, kernel_size=[3, 3], strides=[2, 2]),
+                    AttrDict(filters=16, kernel_size=[3, 3], strides=[2, 2]),
+                    AttrDict(filters=16, kernel_size=[3, 3], strides=[2, 2]),
+                ],
+                deconv_params=[
+                    AttrDict(filters=16, kernel_size=[3, 3], strides=[2, 2]),
+                    AttrDict(filters=16, kernel_size=[3, 3], strides=[2, 2]),
                 ],
                 rnn_params=[
                     AttrDict(sequence_length=5, units=256),
                     AttrDict(sequence_length=10, units=256),
-                ],
-                deconv_params=[
-                    AttrDict(filters=8, kernel_size=[3, 3], strides=[2, 2]),
-                    AttrDict(filters=8, kernel_size=[3, 3], strides=[2, 2]),
                 ],
                 data_format=args.data_format
             ),
@@ -150,16 +148,18 @@ def main(unused_argv):
 
             if args.data_format == "channels_first":
                 image = np.transpose(image, [1, 2, 0])
-                attention_maps = np.transpose(attention_maps, [0, 2, 3, 1])
+                attention_maps = np.transpose(attention_maps, [0, 1, 3, 4, 2])
 
-            for attention_maps in attention_maps:
+            for attention_maps_ in attention_maps:
 
-                attention_map = (attention_map - attention_map.min()) / (attention_map.max() - attention_map.min())
-                attention_map[attention_map < 0.5] = 0.0
-                attention_map = cv2.resize(attention_map, image.shape[:-1])
-                image[:, :, -1] += attention_map
+                for attention_map in attention_maps_:
 
-            cv2.imwrite("outputs/synth90k/attention_map_{}.jpg".format(i), image * 255.)
+                    attention_map = (attention_map - attention_map.min()) / (attention_map.max() - attention_map.min())
+                    attention_map[attention_map < 0.5] = 0.0
+                    attention_map = cv2.resize(attention_map, image.shape[:-1])
+                    image[:, :, -1] += attention_map
+
+            cv2.imwrite("outputs/multi_synth90k/attention_map_{}.jpg".format(i), image * 255.)
 
 
 if __name__ == "__main__":
