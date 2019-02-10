56ad719cb16d25c28be3243091aed54dd472a02b
Hiroki Sakuma <hirokisakuma@HirokinoMacBook-puro.local>
2019-02-10 22:00:23 +0900

    update

--- synth90k_main.py
+++ synth90k_main.py
@@ -165,7 +165,7 @@ if __name__ == "__main__":
     study = optuna.create_study(
         pruner=optuna.pruners.MedianPruner(
             n_startup_trials=0,
-            n_warmup_steps=args.max_steps // 10
+            n_warmup_steps=0
         )
     )
 
