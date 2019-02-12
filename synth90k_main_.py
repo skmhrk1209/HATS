aba92c60d9260077d96832a6749087f2ac091e32
Hiroki Sakuma <hirokisakuma@HirokinoMacBook-puro.local>
2019-02-13 02:09:16 +0900

    update

--- synth90k_main_.py
+++ synth90k_main_.py
@@ -64,7 +64,7 @@ if __name__ == "__main__":
                     AttrDict(filters=8, kernel_size=[3, 3], strides=[2, 2]),
                 ],
                 rnn_params=[
-                    AttrDict(sequence_length=23, hidden_units=256, output_units=(256 >> 4) ** 2 * 8),
+                    AttrDict(max_seq_len=23, num_units=256),
                 ],
                 deconv_params=[
                     AttrDict(filters=8, kernel_size=[3, 3], strides=[2, 2]),
