56dba27c78dd7969d861e79d354479141fc2545e
Hiroki Sakuma <hirokisakuma@HirokinoMacBook-puro.local>
2019-02-13 05:16:36 +0900

    update

--- models/hats_.py
+++ models/hats_.py
@@ -28,6 +28,7 @@ class HATS(object):
         attention_maps = self.attention_network(
             inputs=feature_maps,
             labels=labels,
+            classes=self.classes,
             training=mode == tf.estimator.ModeKeys.TRAIN
         )
 
