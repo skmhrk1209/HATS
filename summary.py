a9e50082f28d1230d01cc0db5e3c02dd40a76867
Hiroki Sakuma <hirokisakuma@HirokinoMacBook-puro.local>
2019-02-13 07:26:48 +0900

    update

--- summary.py
+++ summary.py
@@ -4,13 +4,15 @@ import re
 
 def any(tensor, data_format=None, **kwargs):
 
+    name = re.sub(":.*", "", tensor.name)
+
     if len(tensor.shape) == 1:
 
-        tf.summary.scalar(re.sub(":.*", "", tensor.name), tensor, **kwargs)
+        tf.summary.scalar(name, tensor, **kwargs)
 
     if len(tensor.shape) == 4:
 
         if data_format and data_format == "channels_first":
             tensor = tf.transpose(tensor, [0, 2, 3, 1])
 
-        tf.summary.image(re.sub(":.*", "", tensor.name), tensor, **kwargs)
+        tf.summary.image(name, tensor, **kwargs)
