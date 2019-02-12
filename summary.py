65f8f667362ce94d4068ab2ccf18161fbc57bc37
Hiroki Sakuma <hirokisakuma@HirokinoMacBook-puro.local>
2019-02-13 02:44:30 +0900

    update

--- summary.py
+++ summary.py
@@ -1,7 +1,7 @@
 import tensorflow as tf
 
 
-def any(tensor, **kwargs):
+def any(tensor, data_format=None, **kwargs):
 
     if len(tensor.shape) == 1:
 
@@ -9,7 +9,7 @@ def any(tensor, **kwargs):
 
     if len(tensor.shape) == 4:
 
-        if kwargs.get("data_format") == "channels_first":
+        if data_format and data_format == "channels_first":
             tensor = tf.transpose(tensor, [0, 2, 3, 1])
 
         tf.summary.image(tensor.name, tensor, **kwargs)
