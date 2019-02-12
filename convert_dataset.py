a05ca1bbe941858075bb693dd013b022c8f1dedf
Hiroki Sakuma <hirokisakuma@HirokinoMacBook-puro.local>
2019-02-13 06:47:40 +0900

    update

--- convert_dataset.py
+++ convert_dataset.py
@@ -38,8 +38,6 @@ def main(input_filename, output_filename, num_words, num_chars):
                 chars = map_innermost_element(lambda char: class_ids[char], chars)
                 chars = flatten_innermost_element(chars)
 
-                print(max(chars))
-
                 writer.write(
                     record=tf.train.Example(
                         features=tf.train.Features(
