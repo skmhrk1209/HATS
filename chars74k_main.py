176b13126fe7936454bc0529f1dcaf88c28cad80
Hiroki Sakuma <hirokisakuma@HirokinoMacBook-puro.local>
2019-02-12 22:44:07 +0900

    update

--- chars74k_main.py
+++ chars74k_main.py
@@ -10,37 +10,39 @@
 # max steps: 10000 batch size: 128
 # =============================================================
 
-
 import tensorflow as tf
+import optuna
 import argparse
+import functools
+import dataset
 from attrdict import AttrDict
-from dataset import Dataset
-from models.classifier import Classifier
+from models.hats import HATS
+from networks.attention_network import AttentionNetwork
 from networks.pyramid_resnet import PyramidResNet
 from algorithms import *
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--model_dir", type=str, default="chars74k_classifier", help="model directory")
+parser.add_argument("--model_dir", type=str, default="chars74k_hats_model", help="model directory")
 parser.add_argument("--pretrained_model_dir", type=str, default="", help="pretrained model directory")
-parser.add_argument('--filenames', type=str, nargs="+", default=["chars74k_train.tfrecord"], help="tfrecord filenames")
-parser.add_argument("--num_epochs", type=int, default=1000, help="number of training epochs")
-parser.add_argument("--batch_size", type=int, default=128, help="batch size")
-parser.add_argument("--data_format", type=str, default="channels_first", help="data format")
-parser.add_argument("--steps", type=int, default=None, help="number of training epochs")
-parser.add_argument("--max_steps", type=int, default=None, help="maximum number of training epochs")
-parser.add_argument("--train", action="store_true", help="with training")
-parser.add_argument("--eval", action="store_true", help="with evaluation")
-parser.add_argument("--predict", action="store_true", help="with prediction")
-parser.add_argument("--gpu", type=str, default="0,1,2", help="gpu id")
+parser.add_argument('--train_filenames', type=str, nargs="+", default=["chars74k_train.tfrecord"], help="tfrecords for training")
+parser.add_argument('--val_filenames', type=str, nargs="+", default=["chars74k_val.tfrecord"], help="tfrecords for validation")
+parser.add_argument('--test_filenames', type=str, nargs="+", default=["chars74k_test.tfrecord"], help="tfrecords for test")
+parser.add_argument("--batch_size", type=int, default=100, help="batch size")
 parser.add_argument("--random_seed", type=int, default=1209, help="random seed")
+parser.add_argument("--data_format", type=str, default="channels_first", help="data format")
+parser.add_argument("--max_steps", type=int, default=10000, help="maximum number of training steps")
+parser.add_argument("--steps", type=int, default=None, help="number of evaluation steps")
+parser.add_argument('--train', action="store_true", help="with training")
+parser.add_argument('--eval', action="store_true", help="with evaluation")
+parser.add_argument("--gpu", type=str, default="0", help="gpu id")
 args = parser.parse_args()
 
 tf.logging.set_verbosity(tf.logging.INFO)
 
 
-def main(unused_argv):
+if __name__ == "__main__":
 
-    classifier = tf.estimator.Estimator(
+    estimator = tf.estimator.Estimator(
         model_fn=lambda features, labels, mode: Classifier(
             backbone_network=PyramidResNet(
                 conv_param=AttrDict(filters=64, kernel_size=[7, 7], strides=[2, 2]),
@@ -56,55 +58,57 @@ def main(unused_argv):
             num_classes=37,
             data_format=args.data_format,
             hyper_params=AttrDict(
-                learning_rate=0.001,
-                beta1=0.9,
-                beta2=0.999
+                optimizer=tf.train.AdamOptimizer()
             )
         )(features, labels, mode),
         model_dir=args.model_dir,
         config=tf.estimator.RunConfig(
             tf_random_seed=args.random_seed,
+            save_summary_steps=100,
+            save_checkpoints_steps=100,
             session_config=tf.ConfigProto(
                 gpu_options=tf.GPUOptions(
                     visible_device_list=args.gpu,
                     allow_growth=True
                 )
             )
+        ),
+        warm_start_from=tf.estimator.WarmStartSettings(
+            ckpt_to_initialize_from=args.pretrained_model_dir,
+            vars_to_warm_start=".*pyramid_resnet.*"
         )
     )
 
     if args.train:
 
-        classifier.train(
-            input_fn=Dataset(
-                filenames=args.filenames,
-                num_epochs=args.num_epochs,
+        estimator.train(
+            input_fn=functools.partial(
+                dataset.input_fn,
+                filenames=args.train_filenames,
                 batch_size=args.batch_size,
-                sequence_lengths=[],
+                num_epochs=None,
+                shuffle=True,
+                encoding="png",
                 image_size=[128, 128],
                 data_format=args.data_format,
-                encoding="png"
+                seq_lens=[]
             ),
-            steps=args.steps,
             max_steps=args.max_steps
         )
 
     if args.eval:
 
-        eval_results = classifier.evaluate(
-            input_fn=Dataset(
-                filenames=args.filenames,
-                num_epochs=1,
+        print(estimator.evaluate(
+            input_fn=functools.partial(
+                dataset.input_fn,
+                filenames=args.test_filenames,
                 batch_size=args.batch_size,
-                sequence_lengths=[],
+                num_epochs=1,
+                shuffle=False,
+                encoding="png",
                 image_size=[128, 128],
                 data_format=args.data_format,
-                encoding="png"
-            )
-        )
-
-        print(eval_results)
-
-
-if __name__ == "__main__":
-    tf.app.run()
+                seq_lens=[]
+            ),
+            steps=args.steps
+        ))
